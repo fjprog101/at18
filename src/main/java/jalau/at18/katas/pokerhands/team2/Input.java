0e5dfb959cd09000c488b88c13423fbba399bd42
Rene Rengel <Rene.Rengel@jala.university>
2022-08-29 09:03:28 -0400

    input class added

---
+++ src/main/java/jalau/at18/katas/pokerhands/team2/Input.java
@@ -0,0 +1,10 @@
+package jalau.at18.katas.pokerhands.team2;
+
+public class Input {
+    public String[] hands;
+
+    public String[] separateHands(String input) {
+        hands[0] = input.split();
+    }
+    
+}
