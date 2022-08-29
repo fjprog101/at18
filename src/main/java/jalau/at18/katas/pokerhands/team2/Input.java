441f121246c1f49d8bbe866668fdae200dcd6767
Libertad Tolaba <Libertad.Tolaba@jala.university>
2022-08-29 09:25:36 -0400

    Improve Input class

--- src/main/java/jalau/at18/katas/pokerhands/team2/Input.java
+++ src/main/java/jalau/at18/katas/pokerhands/team2/Input.java
@@ -3,13 +3,16 @@ package jalau.at18.katas.pokerhands.team2;
 public class Input {
     public String[] hands;
 
-    public String[] separateHands(String input) {
-        System.out.print(input.substring(0,input.length()/2));
-        hands[0] = input.substring(0,input.length()/2);
-        hands[1] = input.substring(input.length()/2,input.length());
-        System.out.print(hands[0]);
-        return hands;
+    public Input(){
+        hands = new String[2];
+    }
 
+    public String[] separateHands(String input) {
+        String[] splitInput = input.split(" ");
+        
+        hands[0] = input.substring(0,input.length()/2);
+        hands[1] = input.substring(input.length()/2 + 1,input.length());
+        return hands;
     }
     
 }
