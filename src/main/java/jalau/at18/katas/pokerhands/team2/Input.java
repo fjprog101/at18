c44ad78ec2d03437613459374c62c0731eb733bd
Alvaro Sivila <alvaro.sivila@jala.university>
2022-08-29 09:14:22 -0400

    Added Funcionality to separe the inputs

--- src/main/java/jalau/at18/katas/pokerhands/team2/Input.java
+++ src/main/java/jalau/at18/katas/pokerhands/team2/Input.java
@@ -4,7 +4,12 @@ public class Input {
     public String[] hands;
 
     public String[] separateHands(String input) {
-        hands[0] = input.split();
+        System.out.print(input.substring(0,input.length()/2));
+        hands[0] = input.substring(0,input.length()/2);
+        hands[1] = input.substring(input.length()/2,input.length());
+        System.out.print(hands[0]);
+        return hands;
+
     }
     
 }
--- src/main/java/jalau/at18/katas/pokerhands/team2/Main.java
+++ src/main/java/jalau/at18/katas/pokerhands/team2/Main.java
@@ -2,7 +2,8 @@ package jalau.at18.katas.pokerhands.team2;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Input input = new Input();
+        System.out.print(input.separateHands("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH")[0]);
     }
-    
+
 }
