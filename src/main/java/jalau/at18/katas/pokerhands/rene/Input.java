75d48843b683844aaaa008de899cd8095fc10f23
Rene Rengel <Rene.Rengel@jala.university>
2022-08-29 13:06:13 -0400

    Create reneTests branch fo tests during the course

---
+++ src/main/java/jalau/at18/katas/pokerhands/rene/Input.java
@@ -0,0 +1,13 @@
+package jalau.at18.katas.pokerhands.rene;
+
+public class Input {
+    private String blackHand;
+    private String whiteHand;
+    
+    public Input(String input) {
+            
+    }
+
+    
+    
+}
---
+++ src/main/java/jalau/at18/katas/pokerhands/rene/Main.java
@@ -0,0 +1,8 @@
+package jalau.at18.katas.pokerhands.rene;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+}
