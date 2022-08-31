9f9ef6a175c1717b4609d26f785995b0a7a2671e
Maria Hurtado <Maria.Hurtado@jala.university>
2022-08-31 19:27:28 -0400

    Add convertOne unit test in ConvertNumberTest and ConvertNumber

--- src/main/java/jalau/at18/katas/bankocr/maria/ConvertNumber.java
+++ src/main/java/jalau/at18/katas/bankocr/maria/ConvertNumber.java
@@ -2,7 +2,12 @@ package jalau.at18.katas.bankocr.maria;
 
 public class ConvertNumber {
     public int convert(String firstColumn, String secondColumn, String thirdColumn) {
-        return 0;
+        int number = 0;
+        if (firstColumn.equals(" _ ") && secondColumn.equals("| |") & thirdColumn.equals("|_|")) {
+            number = 0;
+        } else if (firstColumn.equals("   ") && secondColumn.equals("  |") & thirdColumn.equals("  |")) {
+            number = 1;
+        }
+        return number;
     }
-
 }
