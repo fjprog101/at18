a417e539e8918e628f6147b84e547a1e82453d5c
Alvaro Sivila <alvaro.sivila@jala.university>
2022-08-31 19:41:36 -0400

    added class digit with funcionality get Number

---
+++ src/main/java/jalau/at18/katas/bankocr/alvaro/Digit.java
@@ -0,0 +1,17 @@
+package jalau.at18.katas.bankocr.alvaro;
+
+public class Digit {
+    private static final int DIMENSION_MATRIX = 3;
+    private char[][] mat3x3 = new char[DIMENSION_MATRIX][DIMENSION_MATRIX];
+    Number number;
+    public Digit(char[][] matrix) {
+        this.mat3x3 = matrix;
+        convertToNumber();
+    }
+    public void convertToNumber() {
+        number = new Number(mat3x3);
+        number.compareNumber();
+        System.out.print(number.getNumber());
+    }
+
+}
