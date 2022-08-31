e8bb2fbefa6bbbb59ffba18c29777ccfaee22b95
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2022-08-31 12:19:57 -0400

    Creation of new unittest

---
+++ src/test/java/jalau/at18/katas/bankocr/NumCharactersTest.java
@@ -0,0 +1,13 @@
+package jalau.at18.katas;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NumCharactersTest {
+    @Test
+    public void shouldSayHello() {
+        
+        assertEquals("Hello World", sample.sayHello());
+    }
+}
