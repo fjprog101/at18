4721456e13350acf91480ad9c07f40db566ce1f9
Adriana Olivera <Adriana.Olivera@jala.university>
2022-09-01 09:12:27 -0400

    Add Main class to run the program, add the task in the build.gradle, edit checkstyle rule LineLeght 150 to 250 for introduce the intput

---
+++ src/main/java/jalau/at18/katas/bankocr/adriana/Main.java
@@ -0,0 +1,13 @@
+package jalau.at18.katas.bankocr.adriana;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] strin1 = {" ", "_", " ", " ", " ", " ", " ", "_", " ", " ", "_", " ", " ", "_", " ", " ", "_", " ", " ", "_", " ", " ", "_", " ", " ", "_", " "};
+        String[] strin2 = {" ", "_", "|", " ", " ", "|", " ", "_", "|", "|", " ", "|", "|", " ", "|", "|", " ", "|", "|", " ", "|", "|", " ", "|", "|", " ", "|"};
+        String[] strin3 = {" ", "_", "|", " ", " ", "|", "|", "_", " ", "|", "_", "|", "|", "_", "|", "|", "_", "|", "|", "_", "|", "|", "_", "|", "|", "_", "|"};
+        String[][] entry = {strin1, strin2, strin3};
+        Input input = new Input(entry);
+        Account account = new Account(input.getDigits());
+        System.out.println(account.getAccountNumber());
+    }
+}
