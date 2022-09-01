7344324590318692c286d0bb203ec6ffcfce6b8f
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-01 19:05:04 -0400

    refactor to unitest for splitnumber class

--- src/test/java/jalau/at18/katas/bankocr/alvaro/SplitNumberTest.java
+++ src/test/java/jalau/at18/katas/bankocr/alvaro/SplitNumberTest.java
@@ -1,19 +1,30 @@
 package jalau.at18.katas.bankocr.alvaro;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 public class SplitNumberTest {
+
     @Test
-    public void itShouldReturnMatrix() {
-        String[] data = {"                           ",
+    public void itShouldReturnNumbers() {
+        String[] inputData = {"                           ",
                          "|  |  |  |  |  |  |  |  |  ",
                          "|  |  |  |  |  |  |  |  |  ",
                          "                           "};
-        SplitNumber split = new SplitNumber(data);
-        char[][] expectedResult = new char[][]{{' ',' ',' '},
-                                               {'|',' ',' '},
-                                               {'|',' ',' '}};
-        split.extractNumberIn3x3Matrix(0);
-        assertArrayEquals(expectedResult,split.extractNumberIn3x3Matrix(0));
+        SplitNumber split = new SplitNumber(inputData);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        assertEquals(numbers, split.getNumbers());
     }
+
 }
