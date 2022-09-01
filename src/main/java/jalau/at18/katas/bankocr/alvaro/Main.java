224e89c5d7d629fdc877ccb3a5d9ad3a75d15afd
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-01 19:07:58 -0400

    Rename variable data to inputData

--- src/main/java/jalau/at18/katas/bankocr/alvaro/Main.java
+++ src/main/java/jalau/at18/katas/bankocr/alvaro/Main.java
@@ -3,11 +3,10 @@ package jalau.at18.katas.bankocr.alvaro;
 public class Main {
     public static void main(String[] args) {
 
-        String[] data = {"    _  _     _  _  _  _  _ ",
-                         "|   _| _||_||_ |_   ||_| _|",
-                         "|  |_  _|  | _||_|  ||_| _|",
-                         "                           "};
-        SplitNumber split =  new SplitNumber(data);
+        String[] inputData = {"    _  _     _  _  _  _  _ ",
+                              "|   _| _||_||_ |_   ||_||_|",
+                              "|  |_  _|  | _||_|  ||_||_|",
+                              "                           "};
+        SplitNumber split =  new SplitNumber(inputData);
     }
-
 }
