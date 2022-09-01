188d791732f6b5c5aa7b1b856e28054ad1dd6092
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2022-08-31 21:02:11 -0400

    Checkstyle corrections

--- src/main/java/jalau/at18/katas/bankocr/mauricio/Checksum.java
+++ src/main/java/jalau/at18/katas/bankocr/mauricio/Checksum.java
@@ -1,13 +1,4 @@
 package jalau.at18.katas.bankocr.mauricio;
 
 public class Checksum {
-    // AssiginigNumberOnetoThree numToCharOneToThree = new AssiginigNumberOnetoThree();
-    // AssigningNumberFourToSix numToCharFourToSix = new AssigningNumberFourToSix();
-    // AssigningNumberSevenToNine numToCharSevenToNine = new AssigningNumberSevenToNine();
-
-    // public 
-    // ShowFirstThreeNumbers show = new ShowFirstThreeNumbers();
-    // String[][] number1 = numToCharOneToThree.chooseChar(one);
-    // String[][] number2 = numToCharOneToThree.chooseChar(two);
-    // String[][] number3 = numToCharSevenToNine.chooseChar(nine);
 }
