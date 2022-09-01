93a76d67dbd825ee844ba6244285d6324377ef13
Carlos Calderon <Carlos.Calderon@Jala.University>
2022-09-01 00:02:08 -0400

    Added class NumberToSign

---
+++ src/main/java/jalau/at18/katas/bankocr/carlos/Main.java
@@ -0,0 +1,9 @@
+package jalau.at18.katas.bankocr.carlos;
+
+public class Main {
+    static String [] arreglo;
+    public static void main(String[] Args) {
+    NumberToSign prueba = new NumberToSign();
+    //prueba.printNumber();
+}
+}
