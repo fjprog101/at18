e9e38eee4f24dbd4054f7548ec36eb4dcda4cb0c
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-01 19:00:36 -0400

    rename the unitest to readData class

--- src/test/java/jalau/at18/katas/bankocr/alvaro/ReadDataTest.java
+++ src/test/java/jalau/at18/katas/bankocr/alvaro/ReadDataTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 public class ReadDataTest {
     @Test
-    public void itShouldReturnMatrix() {
+    public void itShouldReturnData() {
         String[] data = {"                           ",
                          "|  |  |  |  |  |  |  |  |  ",
                          "|  |  |  |  |  |  |  |  |  ",
