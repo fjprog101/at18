fe3f4925a68774dc94dc552f4ea5f4dd29eeb9a5
FernandezRobert <soniic9911@gmail.com>
2022-08-31 22:55:38 -0400

    Added Mapper Class and its test, also refactored Table

--- src/main/java/jalau/at18/katas/bankocr/robertof/Table.java
+++ src/main/java/jalau/at18/katas/bankocr/robertof/Table.java
@@ -1,7 +1,7 @@
 package jalau.at18.katas.bankocr.robertof;
 
 public class Table {
-    private final int[][] table = {{1,2,3},{6,5,4},{7,8,9}};
+    private final int[][] table = {{1, 2, 3}, {6, 5, 4}, {7, 8, 9}};
     public int getValue(int x, int y) {
         return table[x][y];
     }
