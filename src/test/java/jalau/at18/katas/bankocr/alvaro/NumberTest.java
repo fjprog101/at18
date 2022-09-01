950f74c91dddf586a59d36b0010f70d628be5cfe
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-01 18:38:52 -0400

    Added unitest to return number and value

--- src/test/java/jalau/at18/katas/bankocr/alvaro/NumberTest.java
+++ src/test/java/jalau/at18/katas/bankocr/alvaro/NumberTest.java
@@ -1,76 +1,14 @@
 package jalau.at18.katas.bankocr.alvaro;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 public class NumberTest {
     @Test
-    public void itShouldReturnNumberZero() {
-        char[][]matrix = new char[][]{{' ', '_', ' '}, {'|', ' ', '|'}, {'|', '_', '|'}};
-        Number number = new Number(matrix);
-        number.compareNumber();
+    public void itShouldGetNumber() {
+        char[][]zero = new char[][]{{' ', '_', ' '}, {'|', ' ', '|'}, {'|', '_', '|'}};
+        Number number = new Number(zero, 0);
         assertEquals(0, number.getNumber());
-    }
-    @Test
-    public void itShouldReturnNumberOne() {
-        char[][]matrix = new char[][]{{' ', ' ', ' '}, {'|', ' ', ' '}, {'|', ' ', ' '}};
-        Number number = new Number(matrix);
-        number.compareNumber();
-        assertEquals(1, number.getNumber());
-    }
-    @Test
-    public void itShouldReturnNumberTwo() {
-        char[][]matrix = new char[][]{{' ', '_', ' '}, {' ', '_', '|'}, {'|', '_', ' '}};
-        Number number = new Number(matrix);
-        number.compareNumber();
-        assertEquals(2, number.getNumber());
-    }
-    @Test
-    public void itShouldReturnNumberThree() {
-        char[][]matrix = new char[][]{{' ', '_', ' '}, {' ', '_', '|'}, {' ', '_', '|'}};
-        Number number = new Number(matrix);
-        number.compareNumber();
-        assertEquals(3, number.getNumber());
-    }
-    @Test
-    public void itShouldReturnNumberFour() {
-        char[][]matrix = new char[][]{{' ', ' ', ' '}, {'|', '_', '|'}, {' ', ' ', '|'}};
-        Number number = new Number(matrix);
-        number.compareNumber();
-        assertEquals(4, number.getNumber());
-    }
-    @Test
-    public void itShouldReturnNumberFive() {
-        char[][]matrix = new char[][]{{' ', '_', ' '}, {'|', '_', ' '}, {' ', '_', '|'}};
-        Number number = new Number(matrix);
-        number.compareNumber();
-        assertEquals(5, number.getNumber());
-    }
-    @Test
-    public void itShouldReturnNumberSix() {
-        char[][]matrix = new char[][]{{' ', '_', ' '}, {'|', '_', ' '}, {'|', '_', '|'}};
-        Number number = new Number(matrix);
-        number.compareNumber();
-        assertEquals(6, number.getNumber());
-    }
-    @Test
-    public void itShouldReturnNumberSeven() {
-        char[][]matrix = new char[][]{{' ', '_', ' '}, {' ', ' ', '|'}, {' ', ' ', '|'}};
-        Number number = new Number(matrix);
-        number.compareNumber();
-        assertEquals(7, number.getNumber());
-    }
-    @Test
-    public void itShouldReturnNumberEight() {
-        char[][]matrix = new char[][]{{' ', '_', ' '}, {'|', '_', '|'}, {'|', '_', '|'}};
-        Number number = new Number(matrix);
-        number.compareNumber();
-        assertEquals(8, number.getNumber());
-    }
-    @Test
-    public void itShouldReturnNumberNine() {
-        char[][]matrix = new char[][]{{' ', '_', ' '}, {'|', '_', '|'}, {' ', '_', '|'}};
-        Number number = new Number(matrix);
-        number.compareNumber();
-        assertEquals(9, number.getNumber());
+        assertArrayEquals(zero, number.getData());
     }
 }
