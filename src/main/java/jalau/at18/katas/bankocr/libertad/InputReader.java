3cfbe235f345c89066fc24fdf13ec1d5c425e6f1
Libertad Tolaba <Libertad.Tolaba@jala.university>
2022-09-02 07:57:43 -0400

    Separated InputReader class in other one to specific manage input numbers

--- src/main/java/jalau/at18/katas/bankocr/libertad/InputReader.java
+++ src/main/java/jalau/at18/katas/bankocr/libertad/InputReader.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 public class InputReader {
-    private static final int MAX_NUMBER_CHARACTER = 27;
-    private static final int MAX_COL_NUMBER = 3;
     private File file;
     public InputReader(File file) {
         this.file = file;
@@ -22,24 +20,5 @@ public class InputReader {
         }
         bufferedReader.close();
         return lines.toArray(new String[]{});
-    }
-    public List<String[]> getSeparateHasNumbers() throws IOException {
-        int posIni = 0;
-        int posFinal = MAX_COL_NUMBER;
-        List<String[]> listInputNumbers = new LinkedList<>();
-        while (posFinal <= MAX_NUMBER_CHARACTER) {
-            listInputNumbers.add(getNumber(posIni, posFinal));
-            posIni += MAX_COL_NUMBER;
-            posFinal += MAX_COL_NUMBER;
-        }
-        return listInputNumbers;
-    }
-    private String[] getNumber(int startPos, int finalPos) throws IOException {
-        String[] lines = getLinesOfInput();
-        String firtsLine = lines[0].substring(startPos, finalPos);
-        String secondLine = lines[1].substring(startPos, finalPos);
-        String thridLine = lines[2].substring(startPos, finalPos);
-        String[] number = {firtsLine, secondLine, thridLine};
-        return number;
     }
 }
---
+++ src/test/java/jalau/at18/katas/bankocr/libertadTest/InputNumberTest.java
@@ -0,0 +1,58 @@
+package jalau.at18.katas.bankocr.libertadTest;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+import jalau.at18.katas.bankocr.libertad.InputNumber;
+
+public class InputNumberTest {
+    @Test
+    public void getArrayOfString() throws IOException {
+        File file = new File("C:\\Users\\livia\\Jala\\Prog101\\at18\\src\\main\\java\\jalau\\at18\\katas\\bankocr\\libertad\\inputs\\FirstCase.txt");
+        InputNumber inputNumber = new InputNumber(file);
+        String[] one = {"   ",
+                        "  |",
+                        "  |"};
+        String[] two = {" _ ",
+                        " _|",
+                        "|_ "};
+        String[] three = {" _ ",
+                          " _|",
+                          " _|"};
+        String[] four = {"   ",
+                         "|_|",
+                         "  |"};
+        String[] five = {" _ ",
+                         "|_ ",
+                         " _|"};
+        String[] six = {" _ ",
+                        "|_ ",
+                        "|_|"};
+        String[] seven = {" _ ",
+                          "  |",
+                          "  |"};
+        String[] eigth = {" _ ",
+                          "|_|",
+                          "|_|"};
+        String[] nine = {" _ ",
+                         "|_|",
+                         " _|"};
+        List<String[]> expected = new LinkedList<>();
+        expected.add(one);
+        expected.add(two);
+        expected.add(three);
+        expected.add(four);
+        expected.add(five);
+        expected.add(six);
+        expected.add(seven);
+        expected.add(eigth);
+        expected.add(nine);
+        assertArrayEquals(expected.toArray(), inputNumber.getSeparateHasNumbers().toArray());
+    }
+}
