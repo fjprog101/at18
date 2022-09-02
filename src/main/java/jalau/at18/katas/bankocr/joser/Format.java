9f394cb1499313dd7f19af646d98b02f0f894084
tonio <JoseRomay@jala.university>
2022-09-02 11:52:47 -0400

    Format function optimization

--- src/main/java/jalau/at18/katas/bankocr/joser/Format.java
+++ src/main/java/jalau/at18/katas/bankocr/joser/Format.java
@@ -2,30 +2,31 @@ package jalau.at18.katas.bankocr.joser;
 
 public class Format {
     private String number;
+    public Check checksum;
 
     public Format(String number) {
         this.number = number;
+        checksum = new Check(this.number);
     }
 
     public String formated() {
-        if (isIll()) {
-            return number + " ERR";
-        }
-
-        return "";
+        if (isIll()) return number ;
+        if (isERR()) return number ;
+        return number;
     }
 
     public boolean isIll() {
         if (this.number.indexOf("?") != -1) {
+            this.number += " ILL";
             return true;
         }
         return false;
     }
 
     public boolean isERR() {
-        Check checksum = new Check(number);
-        if (checksum.checkNumber()) {
-            return true;
+        if (!checksum.checkNumber()) {
+            this.number += " ERR";
+            return false;
         }
         return false;
     }
