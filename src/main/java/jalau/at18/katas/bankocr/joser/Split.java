63b78d30467e1451f78c966f1fd46ac16f6e68c6
tonio <JoseRomay@jala.university>
2022-09-02 09:27:43 -0400

    Correction of Split Class in fields

--- src/main/java/jalau/at18/katas/bankocr/joser/Split.java
+++ src/main/java/jalau/at18/katas/bankocr/joser/Split.java
@@ -3,8 +3,6 @@ package jalau.at18.katas.bankocr.joser;
 public class Split {
     private static final int THREE = 3;
     private String[] scan;
-    private String[] scaNumber;
-
     private int pos;
 
     public Split(String[] scan, int pos) {
@@ -13,10 +11,10 @@ public class Split {
     }
 
     public String[] getScaNumber() {
-        this.scaNumber = new String[THREE];
-        this.scaNumber[0] = this.scan[0].substring(this.pos, this.pos + THREE);
-        this.scaNumber[1] = this.scan[1].substring(this.pos, this.pos + THREE);
-        this.scaNumber[2] = this.scan[2].substring(this.pos, this.pos + THREE);
+        String[] scaNumber = new String[THREE];
+        scaNumber[0] = this.scan[0].substring(this.pos, this.pos + THREE);
+        scaNumber[1] = this.scan[1].substring(this.pos, this.pos + THREE);
+        scaNumber[2] = this.scan[2].substring(this.pos, this.pos + THREE);
         return scaNumber;
     }
 
