97c3e90e2333a7a1d7ea2479c2deb39a948d415a
Hugo Solares <Hugo.Solares@jala.university>
2022-09-05 17:45:18 -0400

    Creating new hugo folder for pokerhands kata

---
+++ src/test/java/jalau/at18/katas/pokerhands/hugo/StraightFlushIdentifierTest.java
@@ -0,0 +1,41 @@
+package jalau.at18.katas.pokerhands.hugo;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class StraightFlushIdentifierTest {
+
+    @Test
+    public void shouldIdentifyStraightFlushHand() {
+        StraightFlushIdentifier identifier = new StraightFlushIdentifier();
+
+        PokerHand straightFlushHand = new PokerHand(new Card[]{
+            new Card(CardValue.TWO, 'C'),
+            new Card(CardValue.THREE, 'C'),
+            new Card(CardValue.FOUR, 'C'),
+            new Card(CardValue.FIVE, 'C'),
+            new Card(CardValue.SIX, 'C'),
+        });
+        assertTrue(identifier.identify(straightFlushHand));
+
+        PokerHand noConsecutiveValues = new PokerHand(new Card[]{
+            new Card(CardValue.TWO, 'C'),
+            new Card(CardValue.FOUR, 'C'),
+            new Card(CardValue.FIVE, 'C'),
+            new Card(CardValue.FIVE, 'C'),
+            new Card(CardValue.SEVEN, 'C'),
+        });
+
+        assertFalse(identifier.identify(noConsecutiveValues));
+        PokerHand noSameSuite = new PokerHand(new Card[]{
+            new Card(CardValue.THREE, 'C'),
+            new Card(CardValue.FOUR, 'C'),
+            new Card(CardValue.FIVE, 'H'),
+            new Card(CardValue.SIX, 'C'),
+            new Card(CardValue.SEVEN, 'C'),
+        });
+        assertFalse(identifier.identify(noSameSuite));
+    }
+}
