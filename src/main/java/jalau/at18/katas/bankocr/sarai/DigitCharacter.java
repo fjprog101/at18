bc5e4dd5a496dfe6c85b77787a68ee0bfd0098f3
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-09-04 21:07:54 -0400

    create method recorrerEntrada in class DigitCharacter.java

--- src/main/java/jalau/at18/katas/bankocr/sarai/DigitCharacter.java
+++ src/main/java/jalau/at18/katas/bankocr/sarai/DigitCharacter.java
@@ -1,8 +1,21 @@
 package jalau.at18.katas.bankocr.sarai;
 
 public class DigitCharacter {
+private final int sizethree = 3;
+private final int sizeArray = 27;
+private String[] matrixSize = new String[sizethree];
+private String[] matrixSize27 = new String[sizeArray];
 
-    public DigitCharacter(String[] firstEntry) {
+
+    public DigitCharacter(String[] matrixSize) {
+        this.matrixSize = matrixSize;
     }
-    
+    public void recorrerEntrada() {
+        for (int index = 0; index < matrixSize27.length; index += 3) {
+            String[] numberEntry = new String[sizethree];
+            numberEntry[0] = matrixSize[0].substring(index, index+3);
+            numberEntry[1] = matrixSize[1].substring(index, index+3);
+            numberEntry[2] = matrixSize[2].substring(index, index+3);
+        }
+    }
 }
--- src/test/java/jalau/at18/katas/bankocr/sarai/DigitCharacterTest.java
+++ src/test/java/jalau/at18/katas/bankocr/sarai/DigitCharacterTest.java
@@ -5,12 +5,17 @@ import org.junit.Test;
 public class DigitCharacterTest {
     @Test
     public void entryNumbers(){
-        String [] firstEntry = {" ", "_", " "," ", "|", " "," ", "_", " "," ", "_", " "," ", " ", " "," ", "_", " "," ", "_", " "," ", "_", " "," ", "_", " "," ", "_", " "};
-       
+        //String [][] firstEntry = {{" ", "_", " "," ", "|", " "," ", "_", " "," ", "_", " "," ", " ", " "," ", "_", " "," ", "_", " "," ", "_", " "," ", "_", " "," ", "_", " "}};
+        //String [] firstEntry = {" ", "_", " "," ", "|", " "," ", "_", " "," ", "_", " "," ", " ", " "," ", "_", " "," ", "_", " "," ", "_", " "," ", "_", " "," ", "_", " "};
+        String [] firstEntry = {"    _  _     _  _  _  _  _ ",
+                                "  | _| _||_||_ |_   ||_||_|",
+                                "  ||_  _|  | _||_|  ||_| _|"};
+        
         DigitCharacter firstNumber = new DigitCharacter(firstEntry);
         
-        //assertEquals(0,firstNumber.getcompareCharacter());
+        assertArrayEquals(123456789,firstNumber);
 
-       
+        //DigitReader secondNumber = new DigitReader(firstEntry);
+        //assertEquals(0,firstLine.getcompareCharacter());
     }
 }
