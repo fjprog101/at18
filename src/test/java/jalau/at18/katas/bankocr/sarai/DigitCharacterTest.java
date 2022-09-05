e17cece599c31b9319e50db285141e32828db8a7
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-09-05 00:58:11 -0400

    Add new tests to DigitCharacterTest.java

--- src/test/java/jalau/at18/katas/bankocr/sarai/DigitCharacterTest.java
+++ src/test/java/jalau/at18/katas/bankocr/sarai/DigitCharacterTest.java
@@ -7,11 +7,28 @@ import org.junit.Test;
 public class DigitCharacterTest {
     @Test
     public void entryNumbers(){
-        //String [] firstEntry = {" ", "_", " "," ", "|", " "," ", "_", " "," ", "_", " "," ", " ", " "," ", "_", " "," ", "_", " "," ", "_", " "," ", "_", " "," ", "_", " "};
         String [] firstEntry = {"    _  _     _  _  _  _  _ ",
                                 "  | _| _||_||_ |_   ||_||_|",
-                                "  ||_  _|  ||_||_|  ||_| _|"};
+                                "  ||_  _|  | _||_|  ||_| _|"};
         DigitCharacter firstNumber = new DigitCharacter(firstEntry);
-        assertEquals("123466789",firstNumber.getfinalAcount(""));
+        assertEquals("123456789",firstNumber.getfinalAcount(""));
+    }
+    @Test
+    public void NumbersZero(){
+        String [] firstEntry = {" _  _  _  _  _  _  _  _  _ ",
+                                "| || || || || || || || || |",
+                                "|_||_||_||_||_||_||_||_||_|"};
+        DigitCharacter firstNumber = new DigitCharacter(firstEntry);
+        assertEquals("000000000",firstNumber.getfinalAcount(""));
+    }        String [] firstEntry = {" _  _  _  _  _  _  _  _  _ ",
+    "|_||_||_||_||_||_||_||_||_|",
+    "|_||_||_||_||_||_||_||_||_|"};
+    @Test
+    public void NumbersEigth(){
+        String [] firstEntry = {" _  _  _  _  _  _  _  _  _ ",
+                                "|_||_||_||_||_||_||_||_||_|",
+                                "|_||_||_||_||_||_||_||_||_|"};
+        DigitCharacter firstNumber = new DigitCharacter(firstEntry);
+        assertEquals("888888888",firstNumber.getfinalAcount(""));
     }
 }
