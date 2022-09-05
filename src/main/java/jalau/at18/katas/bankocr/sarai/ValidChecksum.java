991e5de34f8387d89979edeb730c9140dd04c112
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-09-05 01:17:26 -0400

    fix checkstyle to ValidChecksum.java

--- src/main/java/jalau/at18/katas/bankocr/sarai/ValidChecksum.java
+++ src/main/java/jalau/at18/katas/bankocr/sarai/ValidChecksum.java
@@ -5,9 +5,7 @@ public class ValidChecksum {
     public ValidChecksum(String[] firstEntry) {
     }
 
-    public void valid(){
-
+    public void valid() {
         //d1+2d2+3d3+...+9*d9) mod 11 = 0;
-       
     }
 }
