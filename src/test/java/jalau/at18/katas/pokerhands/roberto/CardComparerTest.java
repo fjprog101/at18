d9cc7121b0d35ccbb08d80550b722daed4c19fe6
FernandezRobert <soniic9911@gmail.com>
2022-09-05 18:25:42 -0400

    Fixed imports for tests

--- src/test/java/jalau/at18/katas/pokerhands/roberto/CardComparerTest.java
+++ src/test/java/jalau/at18/katas/pokerhands/roberto/CardComparerTest.java
@@ -1,56 +1,53 @@
 package jalau.at18.katas.pokerhands.roberto;
 
-import jalau.at18.katas.pokerhands.joseg.Card;
-import jalau.at18.katas.pokerhands.joseg.CardComparer;
-import jalau.at18.katas.pokerhands.joseg.CardValue;
-import org.junit.Test;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 public class CardComparerTest {
 
     @Test
     public void shouldSayIfTwoCardsHaveSameValue() {
-        jalau.at18.katas.pokerhands.joseg.CardComparer comparer = new jalau.at18.katas.pokerhands.joseg.CardComparer();
+        CardComparer comparer = new CardComparer();
 
-        jalau.at18.katas.pokerhands.joseg.Card twoSpades = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.TWO, 'S');
-        jalau.at18.katas.pokerhands.joseg.Card twoHearts = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.TWO, 'H');
+        Card twoSpades = new Card(CardValue.TWO, 'S');
+        Card twoHearts = new Card(CardValue.TWO, 'H');
         assertTrue(comparer.haveSameValue(twoSpades, twoHearts));
 
-        jalau.at18.katas.pokerhands.joseg.Card jackHearts1 = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.JACK, 'H');
-        jalau.at18.katas.pokerhands.joseg.Card jackHearts2 = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.JACK, 'H');
+        Card jackHearts1 = new Card(CardValue.JACK, 'H');
+        Card jackHearts2 = new Card(CardValue.JACK, 'H');
         assertTrue(comparer.haveSameValue(jackHearts1, jackHearts2));
 
-        jalau.at18.katas.pokerhands.joseg.Card aceSpades = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.ACE, 'S');
-        jalau.at18.katas.pokerhands.joseg.Card aceHearts = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.ACE, 'H');
+        Card aceSpades = new Card(CardValue.ACE, 'S');
+        Card aceHearts = new Card(CardValue.ACE, 'H');
         assertTrue(comparer.haveSameValue(aceSpades, aceHearts));
 
-        jalau.at18.katas.pokerhands.joseg.Card fiveDiamonds = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.FIVE, 'D');
-        jalau.at18.katas.pokerhands.joseg.Card fiveClubs = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.FIVE, 'C');
+        Card fiveDiamonds = new Card(CardValue.FIVE, 'D');
+        Card fiveClubs = new Card(CardValue.FIVE, 'C');
         assertTrue(comparer.haveSameValue(fiveDiamonds, fiveClubs));
     }
 
     @Test
     public void shouldSayIfTwoCardsDoNotHaveSameValue() {
-        jalau.at18.katas.pokerhands.joseg.CardComparer comparer = new jalau.at18.katas.pokerhands.joseg.CardComparer();
+        CardComparer comparer = new CardComparer();
 
-        jalau.at18.katas.pokerhands.joseg.Card twoSpades = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.TWO, 'S');
-        jalau.at18.katas.pokerhands.joseg.Card jackHearts = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.JACK, 'H');
+        Card twoSpades = new Card(CardValue.TWO, 'S');
+        Card jackHearts = new Card(CardValue.JACK, 'H');
         assertFalse(comparer.haveSameValue(twoSpades, jackHearts));
 
-        jalau.at18.katas.pokerhands.joseg.Card twoHearts = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.TWO, 'H');
+        Card twoHearts = new Card(CardValue.TWO, 'H');
         assertFalse(comparer.haveSameValue(jackHearts, twoHearts));
     }
 
     @Test
     public void shouldSayIfTwoCardsHaveSameSuit() {
-        jalau.at18.katas.pokerhands.joseg.CardComparer comparer = new jalau.at18.katas.pokerhands.joseg.CardComparer();
+        CardComparer comparer = new CardComparer();
 
-        jalau.at18.katas.pokerhands.joseg.Card twoSpades = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.TWO, 'S');
-        jalau.at18.katas.pokerhands.joseg.Card threeSpades = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.THREE, 'S');
-        jalau.at18.katas.pokerhands.joseg.Card jackHearts = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.JACK, 'H');
-        jalau.at18.katas.pokerhands.joseg.Card aceHearts = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.ACE, 'H');
+        Card twoSpades = new Card(CardValue.TWO, 'S');
+        Card threeSpades = new Card(CardValue.THREE, 'S');
+        Card jackHearts = new Card(CardValue.JACK, 'H');
+        Card aceHearts = new Card(CardValue.ACE, 'H');
 
         assertTrue(comparer.haveSameSuit(twoSpades, threeSpades));
         assertTrue(comparer.haveSameSuit(jackHearts, aceHearts));
@@ -61,12 +58,12 @@ public class CardComparerTest {
 
     @Test
     public void shouldSayIfRightCardIsConsecutiveToLeftCard() {
-        jalau.at18.katas.pokerhands.joseg.CardComparer comparer = new CardComparer();
-        jalau.at18.katas.pokerhands.joseg.Card twoSpades = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.TWO, 'S');
-        jalau.at18.katas.pokerhands.joseg.Card threeSpades = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.THREE, 'S');
-        jalau.at18.katas.pokerhands.joseg.Card fourDiamonds = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.FOUR, 'D');
-        jalau.at18.katas.pokerhands.joseg.Card jackHearts = new jalau.at18.katas.pokerhands.joseg.Card(jalau.at18.katas.pokerhands.joseg.CardValue.JACK, 'H');
-        jalau.at18.katas.pokerhands.joseg.Card queenClubs = new Card(CardValue.QUEEN, 'C');
+        CardComparer comparer = new CardComparer();
+        Card twoSpades = new Card(CardValue.TWO, 'S');
+        Card threeSpades = new Card(CardValue.THREE, 'S');
+        Card fourDiamonds = new Card(CardValue.FOUR, 'D');
+        Card jackHearts = new Card(CardValue.JACK, 'H');
+        Card queenClubs = new Card(CardValue.QUEEN, 'C');
 
         assertTrue(comparer.areConsecutive(twoSpades, threeSpades));
         assertTrue(comparer.areConsecutive(threeSpades, fourDiamonds));
--- src/test/java/jalau/at18/katas/pokerhands/roberto/CardValueTest.java
+++ src/test/java/jalau/at18/katas/pokerhands/roberto/CardValueTest.java
@@ -1,26 +1,25 @@
 package jalau.at18.katas.pokerhands.roberto;
 
-import jalau.at18.katas.pokerhands.joseg.CardValue;
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class CardValueTest {
 
     @Test
     public void shouldReturnNextCardInOrderByValue() {
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.THREE, jalau.at18.katas.pokerhands.joseg.CardValue.TWO.next());
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.FOUR, jalau.at18.katas.pokerhands.joseg.CardValue.THREE.next());
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.FIVE, jalau.at18.katas.pokerhands.joseg.CardValue.FOUR.next());
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.SIX, jalau.at18.katas.pokerhands.joseg.CardValue.FIVE.next());
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.SEVEN, jalau.at18.katas.pokerhands.joseg.CardValue.SIX.next());
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.EIGHT, jalau.at18.katas.pokerhands.joseg.CardValue.SEVEN.next());
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.NINE, jalau.at18.katas.pokerhands.joseg.CardValue.EIGHT.next());
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.TEN, jalau.at18.katas.pokerhands.joseg.CardValue.NINE.next());
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.JACK, jalau.at18.katas.pokerhands.joseg.CardValue.TEN.next());
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.QUEEN, jalau.at18.katas.pokerhands.joseg.CardValue.JACK.next());
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.KING, jalau.at18.katas.pokerhands.joseg.CardValue.QUEEN.next());
-        assertEquals(jalau.at18.katas.pokerhands.joseg.CardValue.ACE, jalau.at18.katas.pokerhands.joseg.CardValue.KING.next());
+        assertEquals(CardValue.THREE, CardValue.TWO.next());
+        assertEquals(CardValue.FOUR, CardValue.THREE.next());
+        assertEquals(CardValue.FIVE, CardValue.FOUR.next());
+        assertEquals(CardValue.SIX, CardValue.FIVE.next());
+        assertEquals(CardValue.SEVEN, CardValue.SIX.next());
+        assertEquals(CardValue.EIGHT, CardValue.SEVEN.next());
+        assertEquals(CardValue.NINE, CardValue.EIGHT.next());
+        assertEquals(CardValue.TEN, CardValue.NINE.next());
+        assertEquals(CardValue.JACK, CardValue.TEN.next());
+        assertEquals(CardValue.QUEEN, CardValue.JACK.next());
+        assertEquals(CardValue.KING, CardValue.QUEEN.next());
+        assertEquals(CardValue.ACE, CardValue.KING.next());
         assertEquals(null, CardValue.ACE.next());
     }
 }
