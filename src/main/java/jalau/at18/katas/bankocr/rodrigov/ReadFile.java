b9cd89d1f40bedbf2ecd27783e61f26925459de2
Rodrigo Valda <Rodrigo.Valda@jala.university>
2022-09-05 02:36:55 -0400

    rename path about No such file error from github fixed

--- src/main/java/jalau/at18/katas/bankocr/rodrigov/ReadFile.java
+++ src/main/java/jalau/at18/katas/bankocr/rodrigov/ReadFile.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class ReadFile {
     private int cont = 0;
-    private final String path = "D:/AutomationTesting/Prog101/gitRemote/at18/src/main/java/jalau/at18/katas/bankocr/rodrigov/account.txt";
+    private final String path = "src/main/java/jalau/at18/katas/bankocr/rodrigov/account.txt";
     private final int limitLines = 3;
     private String[] lines = new String[limitLines];
 
