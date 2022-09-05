79d21befc91eaf140fcaef645f498803044a952e
Daniela Martinez <Daniela.Martinez@jala.university>
2022-09-05 17:34:42 -0400

    Eliminate the separatedigits classthat it's not more needed

--- src/main/java/jalau/at18/katas/bankocr/daniela/AnalyzedDigit.java
+++ src/main/java/jalau/at18/katas/bankocr/daniela/AnalyzedDigit.java
@@ -2,6 +2,8 @@ package jalau.at18.katas.bankocr.daniela;
 
 abstract class AnalyzedDigit {
 
+    /// This class must correct to use in the cprrect way the abstraction
+
     abstract String hasSameLinesAs1(String line1, int searchminvalue);
 
     abstract String hasSameLinesAs2(String line2, int searchminvalue);
--- src/main/java/jalau/at18/katas/bankocr/daniela/Separatedigits.java
+++ src/main/java/jalau/at18/katas/bankocr/daniela/Separatedigits.java
@@ -1,29 +1,31 @@
-package jalau.at18.katas.bankocr.daniela;
-
-public class Separatedigits extends AnalyzedDigit {
-    private static final int SIZE = 3;
-    private final String firstline;
-    private final String secondline;
-    private final String thirdline;
-
-    public Separatedigits(String line1, String line2, String line3) {
-        this.firstline = line1;
-        this.secondline = line2;
-        this.thirdline = line3;
-    }
-
-    @Override
-    String hasSameLinesAs1(String line1, int searchminvalue) {
-        return line1.substring(searchminvalue, searchminvalue + SIZE);
-    }
-
-    @Override
-    String hasSameLinesAs2(String line2, int searchminvalue) {
-        return line2.substring(searchminvalue, searchminvalue + SIZE);
-    }
-
-    @Override
-    String hasSameLinesAs3(String line3, int searchminvalue) {
-        return line3.substring(searchminvalue, searchminvalue + SIZE);
-    }
-}
+/*
+ * package jalau.at18.katas.bankocr.daniela;
+ * 
+ * public class Separatedigits extends AnalyzedDigit {
+ * private static final int SIZEONEDIGIT = 3;
+ * private final String firstline;
+ * private final String secondline;
+ * private final String thirdline;
+ * 
+ * public Separatedigits(String line1, String line2, String line3) {
+ * this.firstline = line1;
+ * this.secondline = line2;
+ * this.thirdline = line3;
+ * }
+ * 
+ * @Override
+ * String hasSameLinesAs1(String line1, int searchminvalue) {
+ * return line1.substring(searchminvalue, searchminvalue + SIZEONEDIGIT);
+ * }
+ * 
+ * @Override
+ * String hasSameLinesAs2(String line2, int searchminvalue) {
+ * return line2.substring(searchminvalue, searchminvalue + SIZEONEDIGIT);
+ * }
+ * 
+ * @Override
+ * String hasSameLinesAs3(String line3, int searchminvalue) {
+ * return line3.substring(searchminvalue, searchminvalue + SIZEONEDIGIT);
+ * }
+ * }
+ */
