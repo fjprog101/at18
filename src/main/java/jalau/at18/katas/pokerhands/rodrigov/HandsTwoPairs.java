5bfc0b20044b8ce95b3a2101637656a535083c99
Rodrigo Valda <Rodrigo.Valda@jala.university>
2022-09-06 17:25:59 -0400

    Fixed checkstyle errors

--- src/main/java/jalau/at18/katas/pokerhands/rodrigov/HandsTwoPairs.java
+++ src/main/java/jalau/at18/katas/pokerhands/rodrigov/HandsTwoPairs.java
@@ -1,10 +1,10 @@
 package jalau.at18.katas.pokerhands.rodrigov;
 
-public class HandsTwoPairs extends CardsPattern{
+public class HandsTwoPairs extends CardsPattern {
     private boolean twoPairsCondition;
 
     @Override
-    public boolean match(PokerHand hand){
+    public boolean match(PokerHand hand) {
         PairsCounter pairsCounter = new PairsCounter(hand);
         twoPairsCondition = pairsCounter.getArraySize() == 2 ? true : false;
         return twoPairsCondition;
--- src/main/java/jalau/at18/katas/pokerhands/rodrigov/PairsCounter.java
+++ src/main/java/jalau/at18/katas/pokerhands/rodrigov/PairsCounter.java
@@ -6,32 +6,33 @@ import java.util.HashMap;
 public class PairsCounter {
     private ArrayList<CardValue> pairsFoundedArray;
     private PokerHand hand;
-    public PairsCounter(PokerHand hand){
-        this.hand=hand;
+    public PairsCounter(PokerHand hand) {
+        this.hand = hand;
         pairsFoundedArray = new ArrayList<CardValue>();
         getPairs();
     }
 
-    public void getPairs(){
+    public void getPairs() {
         CardValuesCounter counter = new CardValuesCounter(hand);
         CardValuesCount count = counter.getCount();
         HashMap<CardValue, Integer> counterValues = count.getValuesCards();
         fillArray(counterValues);
     }
-    
-    public void fillArray(HashMap<CardValue, Integer> counterValues){
+
+    public void fillArray(HashMap<CardValue, Integer> counterValues) {
         for (CardValue card : counterValues.keySet()) {
-            if (counterValues.get(card) == 2)
+            if (counterValues.get(card) == 2) {
                 pairsFoundedArray.add(card);
+            }
         }
     }
 
-    public int getArraySize(){
+    public int getArraySize() {
         return pairsFoundedArray.size();
     }
 
-    public ArrayList<CardValue> getPairsArray(){
+    public ArrayList<CardValue> getPairsArray() {
         return pairsFoundedArray;
     }
-    
+
 }
