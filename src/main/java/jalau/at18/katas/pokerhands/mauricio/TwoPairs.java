b17c80e850d0fa89ba123cf7ce21bfa3d8b5da27
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2022-09-05 20:04:45 -0400

    Checkstyle mistakes fixed

--- src/main/java/jalau/at18/katas/pokerhands/mauricio/TwoPairs.java
+++ src/main/java/jalau/at18/katas/pokerhands/mauricio/TwoPairs.java
@@ -1,11 +1,11 @@
 package jalau.at18.katas.pokerhands.mauricio;
 
-public class TwoPairs extends CardValuesCounter{
+public class TwoPairs extends CardValuesCounter {
     private final int one = 1;
     private final int two = 2;
-    CardValue value1;
-    CardValue value2;
-    CardValue value3;
+    private CardValue value1;
+    private CardValue value2;
+    private CardValue value3;
 
     public TwoPairs(PokerHand handWith2PairsPerHand, CardValue value1, CardValue value2, CardValue value3) {
         super(handWith2PairsPerHand);
@@ -14,12 +14,12 @@ public class TwoPairs extends CardValuesCounter{
         this.value3 = value3;
     }
 
-    CardValuesCount count = super.getCount();
-    
-    public boolean twoPairs(){
-        boolean valueCount1 = (count.get(value1)==two) ? true : false;
-        boolean valueCount2 = (count.get(value2)==two) ? true : false;
-        boolean valueCount3 = (count.get(value3)==one) ? true : false;
+    private CardValuesCount count = super.getCount();
+
+    public boolean twoPairs() {
+        boolean valueCount1 = (count.get(value1) == two) ? true : false;
+        boolean valueCount2 = (count.get(value2) == two) ? true : false;
+        boolean valueCount3 = (count.get(value3) == one) ? true : false;
         return valueCount1 == valueCount2 == valueCount3;
     }
 }
