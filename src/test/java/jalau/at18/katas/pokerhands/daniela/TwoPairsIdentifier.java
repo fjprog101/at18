b989c9f66b0efa51acedbc98f3cbee8146e8bb46
Daniela Martinez <Daniela.Martinez@jala.university>
2022-09-05 23:02:17 -0400

    Refactor class Twopairidentifier to onli have 5 lines inside the method

--- src/test/java/jalau/at18/katas/pokerhands/daniela/TwoPairsIdentifier.java
+++ src/test/java/jalau/at18/katas/pokerhands/daniela/TwoPairsIdentifier.java
@@ -19,7 +19,7 @@ public class TwoPairsIdentifier {
                 new Card(CardValue.NINE, 'C'),
         });
         // Twopairidentifier twopairs = new Twopairidentifier();
-        assertTrue(twopairs.identify(handWithGroupsOfTwopairs, CardValue.JACK, CardValue.SEVEN, CardValue.NINE));
+        assertTrue(twopairs.identify(handWithGroupsOfTwopairs, CardValue.JACK, CardValue.SEVEN));
     }
 
     @Test
@@ -33,7 +33,7 @@ public class TwoPairsIdentifier {
                 new Card(CardValue.NINE, 'C'),
         });
         // Twopairidentifier twopairs = new Twopairidentifier();
-        assertFalse(twopairs.identify(handWithGroupsOfTwopairs, CardValue.JACK, CardValue.SEVEN, CardValue.NINE));
+        assertFalse(twopairs.identify(handWithGroupsOfTwopairs, CardValue.JACK, CardValue.SEVEN));
     }
 
 }
