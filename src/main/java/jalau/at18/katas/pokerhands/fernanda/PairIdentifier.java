c4c6a8da87438379cf4a0e6334f4c6bff340bc3e
Fernanda <Fernanda.Aguilar@jala.university>
2022-09-07 11:47:05 -0400

    Created pair identifier with tests

---
+++ src/main/java/jalau/at18/katas/pokerhands/fernanda/PairIdentifier.java
@@ -0,0 +1,14 @@
+package jalau.at18.katas.pokerhands.fernanda;
+
+public class PairIdentifier extends Identifier {
+    private PokerHandAnalizer pokerHandAnalizer;
+
+    public PairIdentifier() {
+        pokerHandAnalizer = new PokerHandAnalizer();
+    }
+
+    public boolean identify(PokerHand hand) {
+        Pair pair = new Pair();
+        return pokerHandAnalizer.analize(hand, pair);
+    }
+}
---
+++ src/test/java/jalau/at18/katas/pokerhands/fernanda/PairIdentifierTest.java
@@ -0,0 +1,49 @@
+package jalau.at18.katas.pokerhands.fernanda;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PairIdentifierTest {
+    
+    @Test
+    public void shouldIdentifyPair() {
+        PairIdentifier identifypair = new PairIdentifier();
+        PokerHand handCase1 = new PokerHand(new Card[]{
+            new Card(CardValue.TWO, 'C'),
+            new Card(CardValue.TWO, 'D'),
+            new Card(CardValue.KING, 'H'),
+            new Card(CardValue.FOUR, 'S'),
+            new Card(CardValue.SIX, 'S'),
+        });
+        assertTrue(identifypair.identify(handCase1));
+        PokerHand handCase2 = new PokerHand(new Card[]{
+            new Card(CardValue.TWO, 'C'),
+            new Card(CardValue.ACE, 'D'),
+            new Card(CardValue.FOUR, 'H'),
+            new Card(CardValue.SIX, 'S'),
+            new Card(CardValue.SIX, 'S'),
+        });
+        assertTrue(identifypair.identify(handCase2));
+
+        PokerHand handCase3 = new PokerHand(new Card[]{
+            new Card(CardValue.TWO, 'C'),
+            new Card(CardValue.ACE, 'D'),
+            new Card(CardValue.FOUR, 'H'),
+            new Card(CardValue.TWO, 'S'),
+            new Card(CardValue.SIX, 'S'),
+        });
+        assertTrue(identifypair.identify(handCase3));
+
+        PokerHand handWithoutPair = new PokerHand(new Card[]{
+            new Card(CardValue.TWO, 'C'),
+            new Card(CardValue.THREE, 'D'),
+            new Card(CardValue.QUEEN, 'H'),
+            new Card(CardValue.FOUR, 'S'),
+            new Card(CardValue.SIX, 'S'),
+        });
+        assertFalse(identifypair.identify(handWithoutPair));
+    }
+}
+
