dab2911540c51795042388cac9202fbbe5d9a17b
Adriana Olivera <Adriana.Olivera@jala.university>
2022-09-07 15:47:39 -0400

    Add StraightIdentifier class and its unitTest

---
+++ src/main/java/jalau/at18/katas/pokerhands/adriana/StraightIdentifier.java
@@ -0,0 +1,14 @@
+package jalau.at18.katas.pokerhands.adriana;
+
+public class StraightIdentifier extends HandIdentifier {
+
+    public StraightIdentifier() {
+    }
+
+    @Override
+    public boolean identify(PokerHand hand) {
+        AllSameSuit allSameSuit = new AllSameSuit();
+        ConsecutiveValues consecutiveValues = new ConsecutiveValues();
+        return !allSameSuit.match(hand) && consecutiveValues.match(hand);
+    }
+}
