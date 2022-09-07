b3dbe11fba8c2bf89c465210a41b79f6f95f6f3f
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-07 16:14:06 -0400

    Copied code to continue develop the pokerhands kata

---
+++ src/main/java/jalau/at18/katas/pokerhands/alvaro/PokerHand.java
@@ -0,0 +1,14 @@
+package jalau.at18.katas.pokerhands.joseg;
+
+public class PokerHand {
+
+    private Card[] cards;
+
+    public PokerHand(Card[] cards) {
+        this.cards = cards;
+    }
+
+    public Card[] getCards() {
+        return cards;
+    }
+}
