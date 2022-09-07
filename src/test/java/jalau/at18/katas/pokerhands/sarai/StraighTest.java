301310e818fab72f0b27b66d59413bf11dc7be3f
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-09-07 19:42:06 -0400

    create class to determine if there its a 5 cards with consecutive values in the Pokerhand

---
+++ src/test/java/jalau/at18/katas/pokerhands/sarai/StraighTest.java
@@ -0,0 +1,37 @@
+package jalau.at18.katas.pokerhands.sarai;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class StraighTest {
+
+    @Test
+    public void shouldIdentifyStraightHand() {
+        Straight identifier = new Straight();
+
+        PokerHand straightHand = new PokerHand(new Card[]{
+            new Card(CardValue.TWO, 'C'),
+            new Card(CardValue.THREE, 'D'),
+            new Card(CardValue.FOUR, 'C'),
+            new Card(CardValue.FIVE, 'C'),
+            new Card(CardValue.SIX, 'C'),
+        });
+        assertTrue(identifier.identify(straightHand));
+    }
+
+    @Test
+    public void shouldIdentifyStraightHandFalse() {
+        Straight identifier = new Straight();
+
+        PokerHand noConsecutiveValues = new PokerHand(new Card[]{
+            new Card(CardValue.TWO, 'C'),
+            new Card(CardValue.FOUR, 'D'),
+            new Card(CardValue.FIVE, 'C'),
+            new Card(CardValue.FIVE, 'C'),
+            new Card(CardValue.SEVEN, 'C'),
+        });
+        assertFalse(identifier.identify(noConsecutiveValues));
+    }
+}
