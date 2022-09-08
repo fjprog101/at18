8a92189b3bac441a04a62e44cc389d90cd4ade59
Daniela Martinez <Daniela.Martinez@jala.university>
2022-09-08 13:20:26 -0400

    create a test for a tie game

--- src/test/java/jalau/at18/katas/pokerhands/daniela/WinningTest.java
+++ src/test/java/jalau/at18/katas/pokerhands/daniela/WinningTest.java
@@ -48,4 +48,26 @@ public class WinningTest {
 
         assertEquals("Player white wins", identifywinner.defineWinner(White, Black));
     }
+
+    @Test
+    public void shouldIdentifyATie() {
+        Winning identifywinner = new Winning();
+
+        PokerHand White = new PokerHand(new Card[] {
+                new Card(CardValue.TWO, 'C'),
+                new Card(CardValue.THREE, 'C'),
+                new Card(CardValue.FOUR, 'C'),
+                new Card(CardValue.FIVE, 'C'),
+                new Card(CardValue.SIX, 'C'),
+        });
+        PokerHand Black = new PokerHand(new Card[] {
+                new Card(CardValue.TWO, 'C'),
+                new Card(CardValue.THREE, 'C'),
+                new Card(CardValue.FOUR, 'C'),
+                new Card(CardValue.FIVE, 'C'),
+                new Card(CardValue.SIX, 'C'),
+        });
+
+        assertEquals("it's a tie", identifywinner.defineWinnerinatie(White, Black));
+    }
 }
