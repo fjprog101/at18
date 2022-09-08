0070357b072edd66bf221cacb93115c0fa932ef2
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-09-07 22:56:30 -0400

    Refactor class fullHouse.java

--- src/main/java/jalau/at18/katas/pokerhands/sarai/FullHouse.java
+++ src/main/java/jalau/at18/katas/pokerhands/sarai/FullHouse.java
@@ -1,35 +1,21 @@
 package jalau.at18.katas.pokerhands.sarai;
 
 public class FullHouse extends CardsPattern {
-    private int existingCount = 0;
-    private int existingCountTwo = 0;
+
     private final int pairsnumbersthree = 3;
     private final int pairsnumberstwo = 2;
     private boolean ifFullHouse = false;
-    private boolean ifFullHouseTwo = false;
 
     @Override
     public boolean match(PokerHand hand) {
         CardValuesCounter counter = new CardValuesCounter(hand);
         CardValuesCount count = counter.getCount();
-        for (int index = 0; index < hand.getCards().length; index++) {
-            pairs(hand, count, index);
-        }
         comparer(count);
-        return ifFullHouse == ifFullHouseTwo;
-    }
-    public void pairs(PokerHand hand, CardValuesCount count, int index) {
-        if (count.get(hand.getCards()[index].getValue()) == pairsnumbersthree && count.get(hand.getCards()[index].getValue()) == pairsnumberstwo) {
-            existingCount += 1;
-            existingCountTwo += 1;
-        }
+        return ifFullHouse;
     }
     public void comparer(CardValuesCount count) {
-        if (count.exists(pairsnumbersthree)) {
+        if (count.exists(pairsnumbersthree) && count.exists(pairsnumberstwo)) {
             ifFullHouse = true;
-        }
-        if (count.exists(pairsnumberstwo)) {
-            ifFullHouseTwo = true;
         }
     }
 }
