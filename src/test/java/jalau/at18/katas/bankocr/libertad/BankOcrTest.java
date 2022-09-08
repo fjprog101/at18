984308beb01c0aaafc2c4bbcc2a610a30bb478ee
Libertad Tolaba <Libertad.Tolaba@jala.university>
2022-09-08 16:50:54 -0400

    Added test case when there is a number with a error

--- src/test/java/jalau/at18/katas/bankocr/libertad/BankOcrTest.java
+++ src/test/java/jalau/at18/katas/bankocr/libertad/BankOcrTest.java
@@ -9,11 +9,19 @@ import org.junit.Test;
 
 public class BankOcrTest {
     @Test
-    public void getLinesOfInput() throws IOException {
+    public void shouldIdentifyAllDigit() throws IOException {
         String direction = "src/main/java/jalau/at18/katas/bankocr/libertad/inputs";
         File file = new File(System.getProperty("user.dir")+File.separator+direction+File.separator+"FirstCase.txt");
         InputNumber input = new InputNumber(file);
         BankOcr bankOcr = new BankOcr(input);
         assertEquals("123456789", bankOcr.readNumbers());
+    }
+    @Test
+    public void shouldIdentifyWhenHasANoDigit() throws IOException {
+        String direction = "src/main/java/jalau/at18/katas/bankocr/libertad/inputs";
+        File file = new File(System.getProperty("user.dir")+File.separator+direction+File.separator+"NumbersWithNoDigit.txt");
+        InputNumber input = new InputNumber(file);
+        BankOcr bankOcr = new BankOcr(input);
+        assertEquals("1234?6709", bankOcr.readNumbers());
     }
 }
