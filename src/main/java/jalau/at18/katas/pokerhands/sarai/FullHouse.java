2ffcbb6b138af74dc974247a7d3827276140f2b1
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-09-07 21:36:36 -0400

    create class to determine if there its a 3 cards of the same value and 2 cards pairs in the Pokerhand

---
+++ src/main/java/jalau/at18/katas/pokerhands/sarai/FullHouse.java
@@ -0,0 +1,40 @@
+package jalau.at18.katas.pokerhands.sarai;
+
+public class FullHouse extends CardsPattern {
+    private int existingCount = 0;
+    private int existingCountTwo = 0;
+    private final int pairsnumbersthree = 3;
+    private final int pairsnumberstwo = 2;
+    private boolean ifFullHouse = false;
+    private boolean ifFullHouseTwo = false;
+
+    @Override
+    public boolean match(PokerHand hand) {
+        CardValuesCounter counter = new CardValuesCounter(hand);
+        CardValuesCount count = counter.getCount();
+        for (int index = 0; index < hand.getCards().length; index++) {
+            pairs(hand, count, index);
+            
+        }
+        comparer(count);
+        return ifFullHouse == ifFullHouseTwo; //ambos verdaderos
+    }
+    public void pairs(PokerHand hand, CardValuesCount count, int index) {
+        if (count.get(hand.getCards()[index].getValue()) == pairsnumbersthree) {
+            existingCount += 1;
+        } if (count.get(hand.getCards()[index].getValue()) == pairsnumberstwo) {
+            existingCountTwo += 1;
+        }
+        
+    }
+    public void comparer(CardValuesCount count){
+        if (count.exists(pairsnumbersthree)) {
+            ifFullHouse = true;
+        }
+        if (count.exists(pairsnumberstwo)) {
+            ifFullHouseTwo = true;
+        }
+    }
+}
+
+
