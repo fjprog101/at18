3874332b7688866e4205a0b9bc9b14bbd9d94669
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-08 21:25:44 -0400

    Added method to return number of runs

--- src/main/java/jalau/at18/katas/pokerhands/alvaro/CardValuesCount.java
+++ src/main/java/jalau/at18/katas/pokerhands/alvaro/CardValuesCount.java
@@ -5,15 +5,17 @@ import java.util.HashMap;
 public class CardValuesCount {
 
     private HashMap<CardValue, Integer> valuesCount;
-
+    private int cont = 0;
     public CardValuesCount() {
         valuesCount = new HashMap<CardValue, Integer>();
+
     }
 
     public void add(CardValue cardValue) {
         int existingCount = 0;
         if (valuesCount.containsKey(cardValue)) {
             existingCount = get(cardValue);
+            cont++;
         }
         valuesCount.put(cardValue, existingCount + 1);
     }
@@ -24,5 +26,8 @@ public class CardValuesCount {
 
     public boolean exists(int count) {
         return valuesCount.containsValue(count);
+    }
+    public int getCont() {
+        return cont;
     }
 }
