31d3b3883dfbddd2b0ea5a955c8416c7104e5d61
Jose Guevara <Jose.Guevara@Jala.University>
2022-09-09 10:14:47 -0400

    Refactor PokerHand so it uses a List<> instead of an array

--- src/test/java/jalau/at18/katas/pokerhands/joseg/PokerHandAnalizerTest.java
+++ src/test/java/jalau/at18/katas/pokerhands/joseg/PokerHandAnalizerTest.java
@@ -3,6 +3,8 @@ package jalau.at18.katas.pokerhands.joseg;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class PokerHandAnalizerTest {
@@ -12,33 +14,33 @@ public class PokerHandAnalizerTest {
         PokerHandAnalizer analizer = new PokerHandAnalizer();
         CardsWithSameValue have2CardsWithSameValue = new CardsWithSameValue(2);
 
-        PokerHand handWith2CardsWithSameValue = new PokerHand(new Card[]{
+        PokerHand handWith2CardsWithSameValue = new PokerHand(Arrays.asList(
             new Card(CardValue.TWO, 'C'),
             new Card(CardValue.TWO, 'D'),
             new Card(CardValue.FOUR, 'H'),
             new Card(CardValue.FIVE, 'S'),
-            new Card(CardValue.SIX, 'S'),
-        });
+            new Card(CardValue.SIX, 'S')
+        ));
         assertTrue(analizer.analize(handWith2CardsWithSameValue, have2CardsWithSameValue));
 
         CardsWithSameValue have3CardsWithSameValue = new CardsWithSameValue(3);
-        PokerHand handWith3CardsWithSameValue = new PokerHand(new Card[]{
+        PokerHand handWith3CardsWithSameValue = new PokerHand(Arrays.asList(
             new Card(CardValue.TWO, 'C'),
             new Card(CardValue.THREE, 'D'),
             new Card(CardValue.THREE, 'H'),
             new Card(CardValue.THREE, 'S'),
-            new Card(CardValue.SIX, 'S'),
-        });
+            new Card(CardValue.SIX, 'S')
+        ));
         assertTrue(analizer.analize(handWith3CardsWithSameValue, have3CardsWithSameValue));
 
         CardsWithSameValue have4CardsWithSameValue = new CardsWithSameValue(4);
-        PokerHand handWith4CardsWithSameValue = new PokerHand(new Card[]{
+        PokerHand handWith4CardsWithSameValue = new PokerHand(Arrays.asList(
             new Card(CardValue.TWO, 'C'),
             new Card(CardValue.JACK, 'D'),
             new Card(CardValue.JACK, 'H'),
             new Card(CardValue.JACK, 'C'),
-            new Card(CardValue.JACK, 'S'),
-        });
+            new Card(CardValue.JACK, 'S')
+        ));
         assertTrue(analizer.analize(handWith4CardsWithSameValue, have4CardsWithSameValue));
 
         assertFalse(analizer.analize(handWith2CardsWithSameValue, have3CardsWithSameValue));
@@ -51,13 +53,13 @@ public class PokerHandAnalizerTest {
         PokerHandAnalizer analizer = new PokerHandAnalizer();
         ConsecutiveValues consecutiveValues = new ConsecutiveValues();
 
-        PokerHand hand = new PokerHand(new Card[]{
+        PokerHand hand = new PokerHand(Arrays.asList(
             new Card(CardValue.TWO, 'C'),
             new Card(CardValue.THREE, 'D'),
             new Card(CardValue.FOUR, 'H'),
             new Card(CardValue.FIVE, 'S'),
-            new Card(CardValue.SIX, 'S'),
-        });
+            new Card(CardValue.SIX, 'S')
+        ));
         assertTrue(analizer.analize(hand, consecutiveValues));
     }
 
@@ -65,22 +67,22 @@ public class PokerHandAnalizerTest {
     public void shouldSayIfHandHasAllCardsWithSameSuit() {
         PokerHandAnalizer analizer = new PokerHandAnalizer();
         AllSameSuit allSameSuit = new AllSameSuit();
-        PokerHand allSameSuitHand = new PokerHand(new Card[]{
+        PokerHand allSameSuitHand = new PokerHand(Arrays.asList(
             new Card(CardValue.TWO, 'C'),
             new Card(CardValue.FOUR, 'C'),
             new Card(CardValue.FIVE, 'C'),
             new Card(CardValue.FIVE, 'C'),
-            new Card(CardValue.SIX, 'C'),
-        });
+            new Card(CardValue.SIX, 'C')
+        ));
         assertTrue(analizer.analize(allSameSuitHand, allSameSuit));
 
-        PokerHand notAllSameSuitHand = new PokerHand(new Card[]{
+        PokerHand notAllSameSuitHand = new PokerHand(Arrays.asList(
             new Card(CardValue.TWO, 'C'),
             new Card(CardValue.FOUR, 'S'),
             new Card(CardValue.FIVE, 'S'),
             new Card(CardValue.FIVE, 'H'),
-            new Card(CardValue.SIX, 'C'),
-        });
+            new Card(CardValue.SIX, 'C')
+        ));
         assertFalse(analizer.analize(notAllSameSuitHand, allSameSuit));
     }
 }
--- src/test/java/jalau/at18/katas/pokerhands/joseg/StraightFlushIdentifierTest.java
+++ src/test/java/jalau/at18/katas/pokerhands/joseg/StraightFlushIdentifierTest.java
@@ -3,6 +3,8 @@ package jalau.at18.katas.pokerhands.joseg;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class StraightFlushIdentifierTest {
@@ -11,31 +13,31 @@ public class StraightFlushIdentifierTest {
     public void shouldIdentifyStraightFlushHand() {
         StraightFlushIdentifier identifier = new StraightFlushIdentifier();
 
-        PokerHand straightFlushHand = new PokerHand(new Card[]{
+        PokerHand straightFlushHand = new PokerHand(Arrays.asList(
             new Card(CardValue.TWO, 'C'),
             new Card(CardValue.THREE, 'C'),
             new Card(CardValue.FOUR, 'C'),
             new Card(CardValue.FIVE, 'C'),
-            new Card(CardValue.SIX, 'C'),
-        });
+            new Card(CardValue.SIX, 'C')
+        ));
         assertTrue(identifier.identify(straightFlushHand));
 
-        PokerHand noConsecutiveValues = new PokerHand(new Card[]{
+        PokerHand noConsecutiveValues = new PokerHand(Arrays.asList(
             new Card(CardValue.TWO, 'C'),
             new Card(CardValue.FOUR, 'C'),
             new Card(CardValue.FIVE, 'C'),
             new Card(CardValue.FIVE, 'C'),
-            new Card(CardValue.SEVEN, 'C'),
-        });
+            new Card(CardValue.SEVEN, 'C')
+        ));
 
         assertFalse(identifier.identify(noConsecutiveValues));
-        PokerHand noSameSuite = new PokerHand(new Card[]{
+        PokerHand noSameSuite = new PokerHand(Arrays.asList(
             new Card(CardValue.THREE, 'C'),
             new Card(CardValue.FOUR, 'C'),
             new Card(CardValue.FIVE, 'H'),
             new Card(CardValue.SIX, 'C'),
-            new Card(CardValue.SEVEN, 'C'),
-        });
+            new Card(CardValue.SEVEN, 'C')
+        ));
         assertFalse(identifier.identify(noSameSuite));
     }
 }
