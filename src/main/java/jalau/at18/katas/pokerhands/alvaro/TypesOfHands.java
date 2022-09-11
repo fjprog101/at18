12f3731546d9aab9f83c6d1a6ffb3b8e5b62c75a
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-11 19:20:23 -0400

    Added function to retur name of Hand

--- src/main/java/jalau/at18/katas/pokerhands/alvaro/TypesOfHands.java
+++ src/main/java/jalau/at18/katas/pokerhands/alvaro/TypesOfHands.java
@@ -1,26 +1,31 @@
 package jalau.at18.katas.pokerhands.alvaro;
 
 public enum TypesOfHands {
-    HIGHCARD(new HighCardIdentifier(), 1),
-    PAIR(new PairIdentifier(), 2),
-    TWOPAIR(new TwoPairIdentifier(), 3),
-    THREEOFAKIND(new ThreeOfAKindIdentifier(), 4),
-    STRAIGHT(new StraightIdentifier(), 5),
-    FLUSH(new FlushIdentifier(), 6),
-    FULLHOUSE(new FullHouseIdentifier(), 7),
-    FOUROFAKIND(new FourOfAKindIdentifier(), 8),
-    STRAIGHTFLUSH(new StraightFlushIdentifier(), 9);
+    HIGHCARD(new HighCardIdentifier(), 1, "HighCard"),
+    PAIR(new PairIdentifier(), 2, "Pair"),
+    TWOPAIR(new TwoPairIdentifier(), 3, "TwoPair"),
+    THREEOFAKIND(new ThreeOfAKindIdentifier(), 4, "Three Of a Kind"),
+    STRAIGHT(new StraightIdentifier(), 5, "Straight"),
+    FLUSH(new FlushIdentifier(), 6, "Flush"),
+    FULLHOUSE(new FullHouseIdentifier(), 7, "Full House"),
+    FOUROFAKIND(new FourOfAKindIdentifier(), 8, "Four of a Kind"),
+    STRAIGHTFLUSH(new StraightFlushIdentifier(), 9, "Straight Flush");
 
     private Identifiers identifiers;
     private int rankValue;
-    TypesOfHands(Identifiers identifiers, int rankValue) {
+    private String typeOfHand;
+    TypesOfHands(Identifiers identifiers, int rankValue, String typeOfHand) {
         this.identifiers = identifiers;
         this.rankValue = rankValue;
+        this.typeOfHand = typeOfHand;
     }
     public Identifiers getTypeHand() {
         return identifiers;
     }
     public int getRankValue() {
         return rankValue;
+    }
+    public String getHand() {
+        return typeOfHand;
     }
 }
