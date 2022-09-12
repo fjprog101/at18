22e3905d5b1649d5ae409260330b11eee3e1c8f9
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-09-12 18:54:45 -0400

    Solution for User Story 2

--- src/main/java/jalau/at18/katas/bankocr/sarai/ValidChecksum.java
+++ src/main/java/jalau/at18/katas/bankocr/sarai/ValidChecksum.java
@@ -1,11 +1,32 @@
 package jalau.at18.katas.bankocr.sarai;
 
 public class ValidChecksum {
+    private String account ;
+    //private int sum = 0;
+    public ValidChecksum(String accountNum) { //"123456679"
+        this.account = accountNum;
+    }
+    public String[] value() {
+        String[] arrOfStr = account.split("");
+        return arrOfStr;
+    }
+    public int valid() {
+        int sum = 0;
+        int numer = 9;
+        for (String positionnumber : value()) {
+            sum += (Integer.parseInt(positionnumber) * numer--);
+        }
+        return sum % 11;
 
-    public ValidChecksum(String[] firstEntry) {
+    }
+    public boolean validnumber() {
+        
+        if(valid()== 0){
+        return true;
+        }
+        return false;
+        
     }
 
-    public void valid() {
-        //d1+2d2+3d3+...+9*d9) mod 11 = 0;
-    }
+
 }
--- src/test/java/jalau/at18/katas/bankocr/sarai/ValidChecksumTest.java
+++ src/test/java/jalau/at18/katas/bankocr/sarai/ValidChecksumTest.java
@@ -1,16 +1,31 @@
 package jalau.at18.katas.bankocr.sarai;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class ValidChecksumTest {
     @Test
-    public void NumbersEigth(){
+    public void if_is_a_valid_number() {
+        String [] firstEntry = {"    _  _     _  _  _  _  _ ",
+                                "  | _| _||_||_ |_   ||_||_|",
+                                "  ||_  _|  | _||_|  ||_| _|"};
+        DigitCharacter firstNumber = new DigitCharacter(firstEntry);
+        String accountNum = firstNumber.getfinalAcount(""); //"123456789"
+        ValidChecksum nn = new ValidChecksum(accountNum);
+        assertEquals(0, nn.valid());
+        assertTrue(nn.validnumber());
+    }
+    @Test
+    public void if_is_a_valid_number2() {
         String [] firstEntry = {" _     _  _  _  _  _  _  _ ",
                                 " _||_||_ |_||_| _||_||_ |_ ",
                                 " _|  | _||_||_||_ |_||_| _|"};
-        ValidChecksum firstNumber = new ValidChecksum(firstEntry);
-        //assertEquals("345882865",firstNumber.getfinalAcount(""));
+        DigitCharacter firstNumber = new DigitCharacter(firstEntry);
+        String accountNum = firstNumber.getfinalAcount(""); //"345882865"
+        ValidChecksum nn = new ValidChecksum(accountNum);
+        assertEquals(0, nn.valid());
+        assertTrue(nn.validnumber());
     }
 }
