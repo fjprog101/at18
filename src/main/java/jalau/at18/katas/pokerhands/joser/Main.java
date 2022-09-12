3b34cbf2c32125792479b2d4f9584ae9560c0277
tonio <JoseRomay@jala.university>
2022-09-12 00:38:04 -0400

    Last updated

---
+++ src/main/java/jalau/at18/katas/pokerhands/joser/Main.java
@@ -0,0 +1,26 @@
+package jalau.at18.katas.pokerhands.joser;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PokerHand hand1 = new PokerHand(new Card[] {
+            new Card(CardValue.TWO, 'C'),
+            new Card(CardValue.THREE, 'C'),
+            new Card(CardValue.FOUR, 'C'),
+            new Card(CardValue.FIVE, 'C'),
+            new Card(CardValue.SIX, 'C'),
+        });
+        PokerHand hand2 = new PokerHand(new Card[] {
+            new Card(CardValue.TWO, 'C'),
+            new Card(CardValue.THREE, 'C'),
+            new Card(CardValue.FOUR, 'C'),
+            new Card(CardValue.FIVE, 'C'),
+            new Card(CardValue.SIX, 'C'),
+        });
+
+        Game game= new Game(hand1, hand2);
+        System.out.println(game.getWin()==1? "White wins":"Black wins");
+
+    }
+
+}
--- src/main/java/jalau/at18/katas/pokerhands/joser/PokerHand.java
+++ src/main/java/jalau/at18/katas/pokerhands/joser/PokerHand.java
@@ -1,14 +1,19 @@
 package jalau.at18.katas.pokerhands.joser;
+import java.util.Arrays;
 
 public class PokerHand {
 
     private Card[] cards;
+
 
     public PokerHand(Card[] cards) {
         this.cards = cards;
     }
 
     public Card[] getCards() {
+        Arrays.sort(cards, (cartaA, cartaB) -> cartaB.getValue().compareTo(cartaA.getValue()));
         return cards;
     }
+
+  
 }
