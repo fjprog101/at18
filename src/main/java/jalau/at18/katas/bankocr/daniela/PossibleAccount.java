fac7a210941893fb4fa23dd798c71956f523408b
Daniela Martinez <Daniela.Martinez@jala.university>
2022-09-12 21:00:42 -0400

    Refactor the class StatusAccount because some problems with checkstyle, also refactor the Possible Account for the same reason

--- src/main/java/jalau/at18/katas/bankocr/daniela/PossibleAccount.java
+++ src/main/java/jalau/at18/katas/bankocr/daniela/PossibleAccount.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class PossibleAccount {
     private static final int SIZEONEDIGIT = 3;
     private static final int CAPACITY = 9;
-    private List<DigitValid> digit = new ArrayList<DigitValid>(CAPACITY);
+    private List<DigitValid> digits = new ArrayList<DigitValid>(CAPACITY);
     private List<String> digitpossible = new ArrayList<String>(CAPACITY);
     private SetPossibleDigit possible = new SetPossibleDigit();
 
@@ -18,21 +18,20 @@ public class PossibleAccount {
 
     private void extractDigits(String line1, String line2, String line3, int sizenumbers) {
         for (int searchminvalue = 0; searchminvalue < sizenumbers; searchminvalue += SIZEONEDIGIT) {
-            digit.add(new DigitValid((line1.substring(searchminvalue, searchminvalue + SIZEONEDIGIT)),
+            digits.add(new DigitValid((line1.substring(searchminvalue, searchminvalue + SIZEONEDIGIT)),
                     (line2.substring(searchminvalue, searchminvalue + SIZEONEDIGIT)),
                     (line3.substring(searchminvalue, searchminvalue + SIZEONEDIGIT))));
         }
     }
 
     public String getPossibleDigitChar(int index, char data, String oldNumber, String number) {
-        for (DigitValid digit : digit) {
+        for (DigitValid digit : digits) {
             String posib = digit.getDigitChar();
             for (int indexfinal = 0; posib.length() > 0 && indexfinal < posib.length(); indexfinal++) {
                 digitpossible.add(possible.setPos(index, posib.charAt(indexfinal), number));
                 Set<String> midigits = new HashSet<String>(digitpossible);
                 digitpossible.clear();
                 digitpossible.addAll(midigits);
-
             }
         }
         return digitpossible.toString();
--- src/main/java/jalau/at18/katas/bankocr/daniela/StatusAccount.java
+++ src/main/java/jalau/at18/katas/bankocr/daniela/StatusAccount.java
@@ -2,14 +2,15 @@ package jalau.at18.katas.bankocr.daniela;
 
 public class StatusAccount extends BankAccount {
     private String entry = getDigitChar();
+    private static final int CAPACITY = 9;
 
     public StatusAccount(String line1, String line2, String line3, int sizeValue, int sizenumbers) {
         super(line1, line2, line3, sizenumbers);
     }
 
     public int status(int bad) {
-        for (int i = 0; i < 9; i++) {
-            char result = entry.charAt(i);
+        for (int index = 0; index < CAPACITY; index++) {
+            char result = entry.charAt(index);
             if (result == '?') {
                 bad++;
             }
