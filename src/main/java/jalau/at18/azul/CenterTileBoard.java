b30d8a8ea97d08ae31fa00c328f61ffa08213bf8
Fernanda <Fernanda.Aguilar@jala.university>
2022-09-13 15:09:56 -0400

    Created center tile Board for the tiles that aren't used in the factory and its tests

---
+++ src/main/java/jalau/at18/azul/CenterTileBoard.java
@@ -0,0 +1,28 @@
+package jalau.at18.azul;
+import java.util.*;
+
+public class CenterTileBoard extends ArrayList<Character> {
+
+
+    public CenterTileBoard() {
+        super();
+    }
+
+    public CenterTileBoard removeTilesOfSpecificColor(char color) {
+        for (int position = 0; position < this.size(); ++position) {
+            if (this.get(position) == color) {
+                this.remove(position);
+            }
+        }
+        return this;
+    }
+
+    public CenterTileBoard addTilestoCenter(char tile, int count) {
+        for (int position = 0; position < count; ++position) {
+            this.add(tile);
+        }
+        return this;
+    }
+
+
+}
---
+++ src/test/java/jalau/at18/azul/CenterTileBoardTest.java
@@ -0,0 +1,54 @@
+package jalau.at18.azul;
+
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+
+public class CenterTileBoardTest {
+    @Test
+    public void testRemoveTiles() {
+        CenterTileBoard center = new CenterTileBoard(); 
+        center.add('D');
+        center.add('D');
+        center.add('R');
+        center.add('Y');
+        center.removeTilesOfSpecificColor('R');
+        CenterTileBoard expected = new CenterTileBoard();
+        expected.add('D');
+        expected.add('D');
+        expected.add('Y');
+        assertEquals(expected, center);
+    }
+    @Test
+    public void testRemoveTiles1() {
+        CenterTileBoard center = new CenterTileBoard(); 
+        center.add('D');
+        center.add('R');
+        center.add('R');
+        center.add('Y');
+        center.add('D');
+        center.removeTilesOfSpecificColor('D');
+        CenterTileBoard expected = new CenterTileBoard();
+        expected.add('R');
+        expected.add('R');
+        expected.add('Y');
+        assertEquals(expected, center);
+    }
+
+    @Test
+    public void testaddTiles() {
+        CenterTileBoard center = new CenterTileBoard(); 
+        center.add('R');
+        center.add('R');
+        center.addTilestoCenter('D', 3);
+        CenterTileBoard expected = new CenterTileBoard();
+        expected.add('R');
+        expected.add('R');
+        expected.add('D');
+        expected.add('D');
+        expected.add('D');
+        assertEquals(expected, center);
+    }
+
+}
