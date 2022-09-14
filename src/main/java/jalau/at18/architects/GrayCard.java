7c73f591b1308f10f70b53062763d2ceb6576b7a
Carlos Calderon <Carlos.Calderon@Jala.University>
2022-09-13 22:50:30 -0400

    Added GrayCard and Resource classes with the test

---
+++ src/main/java/jalau/at18/architects/GrayCard.java
@@ -0,0 +1,19 @@
+package jalau.at18.architects;
+
+public class GrayCard extends Card {
+    private Resource resource;
+    private String color;
+    public GrayCard(Resource resource) {
+        this.color = "Gray";
+        this.resource = resource;
+    }
+
+    @Override
+    String getColor() {
+        return color;
+    }
+    Resource getResource() {
+        return resource;
+    }
+
+}
