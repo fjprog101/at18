5918ca351b3d3a9ff48f3cbffcfa48d80cf1aa35
Hugo Solares <Hugo.Solares@jala.university>
2022-09-15 19:10:22 -0400

    Creating a bag class that handles mixed bag, deleting mixedbag class, refactoring and tests

--- src/main/java/jalau/at18/azul/Bag.java
+++ src/main/java/jalau/at18/azul/Bag.java
@@ -1,11 +1,11 @@
 package jalau.at18.azul;
 
 import java.util.ArrayList;
+import java.util.*;
 
 public class Bag {
 
-    private ArrayList<Character> bag;
-    private char[] colors = {'R', 'Y', 'D', 'B', 'W'};
+    private ArrayList<Tile> bag;
     private static final int NUMBER_ONE_COLOR = 20;
     private static final int NUMBER_OF_COLORS = 5;
 
@@ -13,16 +13,23 @@ public class Bag {
         this.bag = new ArrayList<>();
     }
 
-    public ArrayList<Character> fillBagWithColors() {
+    public ArrayList<Tile> fillBagWithColors() {
+        Tile[] color = {Tile.RED, Tile.YELLOW, Tile.DARK, Tile.BLUE, Tile.WHITE};
         for (int index = 0; index < NUMBER_OF_COLORS; index++) {
             for (int jindex = 0; jindex < NUMBER_ONE_COLOR; jindex++) {
-                bag.add(colors[index]);
+                this.bag.add((color[index]));
             }
         }
         return bag;
     }
 
-    public ArrayList<Character> getBag() {
-        return fillBagWithColors();
+    public ArrayList<Tile> getMixedBag() {
+        return mixColors(fillBagWithColors());
+    }
+
+    public ArrayList<Tile> mixColors(ArrayList<Tile> simpleBag) {
+        ArrayList<Tile> mixedBag = simpleBag;
+        Collections.shuffle(mixedBag);
+        return mixedBag;
     }
 }
--- src/main/java/jalau/at18/azul/Tile.java
+++ src/main/java/jalau/at18/azul/Tile.java
@@ -19,5 +19,4 @@ public enum Tile {
     public char getColor() {
         return colorTile;
     }
-
 }
