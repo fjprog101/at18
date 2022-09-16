807030808538702a0a62d61921f4bb9505c92f1d
Carlos Calderon <Carlos.Calderon@Jala.University>
2022-09-15 21:12:37 -0400

    GrayCards class modified getResource and add the testGrayCard

--- src/test/java/jalau/at18/architects/TestGrayCard.java
+++ src/test/java/jalau/at18/architects/TestGrayCard.java
@@ -1,7 +1,10 @@
 package jalau.at18.architects;
 
 import org.junit.Test;
-import org.junit.Assert;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class TestGrayCard {
     
     @Test
