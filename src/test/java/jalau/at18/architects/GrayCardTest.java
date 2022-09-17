22afd71a0947b951c2a8685e1847bee7b19236f9
Carlos Calderon <Carlos.Calderon@Jala.University>
2022-09-16 23:39:37 -0400

    Fixed GrayCardTest

---
+++ src/test/java/jalau/at18/architects/GrayCardTest.java
@@ -0,0 +1,17 @@
+package jalau.at18.architects;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class GrayCardTest {
+    
+    @Test
+    public void cardShouldBeGray() {
+        GrayCard grayCard = new GrayCard(Resource.WATER);
+        assertEquals("Gray",grayCard.getColor());
+        assertNotNull(grayCard.getResource());
+    }
+
+}
