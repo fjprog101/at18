ae1f6d2569823de6bef6bb0f1fea3ca737ad31a9
Daniela Martinez <Daniela.Martinez@jala.university>
2022-09-19 19:50:39 -0400

    Solve some checkstyle problems in the main class and azulframe class

--- src/main/java/jalau/at18/azul/Main.java
+++ src/main/java/jalau/at18/azul/Main.java
@@ -1,10 +1,13 @@
 package jalau.at18.azul;
 
 public class Main {
+    private static final int DEFAULT_POS = 200;
+    private static final int DEFAULT_SIZE = 900;
+    private static final int DEFAULT_SIZE1 = 600;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         AzulFrame player = new AzulFrame();
         player.setVisible(true);
-        player.setBounds(200,200,900,600);
-	}
+        player.setBounds(DEFAULT_POS, DEFAULT_POS, DEFAULT_SIZE, DEFAULT_SIZE1);
+    }
 }
