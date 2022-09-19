766ec65d7937e7ce7e8c3a23a7707db1127ac0ff
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2022-09-19 09:17:19 -0400

    Changes on the cards names and the constants for colors

--- src/main/java/jalau/at18/architects/CardComparatorForResource.java
+++ src/main/java/jalau/at18/architects/CardComparatorForResource.java
@@ -1,9 +1,12 @@
 package jalau.at18.architects;
 
 public class CardComparatorForResource implements Comparator {
+    final String GRAY = "Gray";
+    final String YELLOW = "Yellow";
+
     public boolean compare(Card c1, Card c2) {
-        return ((c1.getColor() == "Gray" && c2.getColor() == "Yellow")
-                || (c1.getColor() == "Yellow" && c2.getColor() == "Gray")
-                || (c1.getColor() == "Yellow" && c2.getColor() == "Yellow"));
+        return ((c1.getColor() == GRAY && c2.getColor() == YELLOW)
+                || (c1.getColor() == YELLOW && c2.getColor() == GRAY)
+                || (c1.getColor() == YELLOW && c2.getColor() == YELLOW));
     }
 }
