8146df3fa5d0999c088a3ae6d1d27a456d167af7
FernandezRobert <soniic9911@gmail.com>
2022-09-19 12:36:38 -0400

    Created the initialization of a frame

---
+++ src/main/java/jalau/at18/kingoftokyo/view/Main.java
@@ -0,0 +1,8 @@
+package jalau.at18.kingoftokyo.view;
+
+public class Main {
+    public static void main(String[] args) {
+        FrameInit frame = new FrameInit("King of Tokyo", 420,420);
+
+    }
+}
