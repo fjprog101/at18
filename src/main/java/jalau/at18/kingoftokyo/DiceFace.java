725514785d88535586c048c50300354f511bd11f
Maria Hurtado <Maria.Hurtado@jala.university>
2022-09-20 16:51:51 -0400

    Udpdate DiceFace to manage icons

--- src/main/java/jalau/at18/kingoftokyo/DiceFace.java
+++ src/main/java/jalau/at18/kingoftokyo/DiceFace.java
@@ -1,22 +1,22 @@
 package jalau.at18.kingoftokyo;
 
 public enum DiceFace {
-    ONE(1, 1),
-    TWO(2, 2),
-    THREE(3, 3),
-    HEALING(4, 1),
-    ENERGY(5, 1),
-    PUNCHING(6, 1);
+    ONE("diceone.png", 1),
+    TWO("dicetwo.png", 2),
+    THREE("dicethree.png", 3),
+    HEALING("dicefourhealing.png", 1),
+    ENERGY("dicefiveenergy.png", 1),
+    PUNDING("dicesixpunching.png", 1);
 
-    private int representativeValue;
+    private String representativeValue;
     private int pointValue;
 
-    DiceFace(int representativeValue, int pointValue) {
+    DiceFace(String representativeValue, int pointValue) {
         this.representativeValue = representativeValue;
         this.pointValue = pointValue;
     }
 
-    public int getRepresentativeValue() {
+    public String getRepresentativeValue() {
         return representativeValue;
     }
 
