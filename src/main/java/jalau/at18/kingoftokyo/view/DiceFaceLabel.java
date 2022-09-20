1be9369157a47bbf13ba80f30d441611dc68b577
Maria Hurtado <Maria.Hurtado@jala.university>
2022-09-20 09:42:30 -0400

    Add DiceFaceLabel, and RollerButton and logic to generated the labels and display the roll dice

---
+++ src/main/java/jalau/at18/kingoftokyo/view/DiceFaceLabel.java
@@ -0,0 +1,13 @@
+package jalau.at18.kingoftokyo.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DiceFaceLabel extends JLabel {
+    public static final int SIZE_FONT = 25;
+     public DiceFaceLabel(String text, int posX, int posY, int width, int height) {
+        super(text);
+        setBounds(posX, posY, width, height);
+        setFont(new Font("Times New Roman", Font.PLAIN, SIZE_FONT));
+    }
+}
