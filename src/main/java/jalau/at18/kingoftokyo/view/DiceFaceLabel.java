34e491506139035c06220279cff96ff2009fe9fa
Maria Hurtado <Maria.Hurtado@jala.university>
2022-09-20 12:04:07 -0400

    Fix checkstyle errors

--- src/main/java/jalau/at18/kingoftokyo/view/DiceFaceLabel.java
+++ src/main/java/jalau/at18/kingoftokyo/view/DiceFaceLabel.java
@@ -5,7 +5,8 @@ import java.awt.*;
 
 public class DiceFaceLabel extends JLabel {
     public static final int SIZE_FONT = 25;
-     public DiceFaceLabel(String text, int posX, int posY, int width, int height) {
+
+    public DiceFaceLabel(String text, int posX, int posY, int width, int height) {
         super(text);
         setBounds(posX, posY, width, height);
         setFont(new Font("Times New Roman", Font.PLAIN, SIZE_FONT));
