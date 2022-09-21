13766b55dceab98033ba6d2b0bdf4bb331a4edf4
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-21 15:25:39 -0400

    changed the class to JcomboBox, and added Actionlistener for event handling

--- src/main/java/jalau/at18/architects/view/ChoiceNumberPlayers.java
+++ src/main/java/jalau/at18/architects/view/ChoiceNumberPlayers.java
@@ -1,16 +1,17 @@
 package jalau.at18.architects.view;
 import javax.swing.JComboBox;
-import javax.swing.JPanel;
 import static jalau.at18.architects.Constants.ComboBox.*;
-public class ChoiceNumberPlayers extends JPanel {
-    private JComboBox<Integer> options;
-    
-    public ChoiceNumberPlayers() {
-        options = new JComboBox<Integer>();
+import java.awt.event.ActionListener;
+public class ChoiceNumberPlayers extends JComboBox {
+
+    private JComboBox<Integer> list;
+    public ChoiceNumberPlayers(ActionListener listener) {
+        list = new JComboBox<>();
         setBounds(POS_X, POS_Y, WIDTH_CHOICE, HEIGHT_CHOICE);
-        options.addItem(TWO);
-        options.addItem(THREE);
-        options.addItem(FOUR);
-        add(options);
+        addItem(TWO);
+        addItem(THREE);
+        addItem(FOUR);
+        add(list);
+        addActionListener(listener);
     }
 }
