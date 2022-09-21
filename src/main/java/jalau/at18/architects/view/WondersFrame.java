0aad3b7cc5b29b5a6922ea36d350563b978694bb
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-21 07:52:33 -0400

    added the different elements to the main frame

--- src/main/java/jalau/at18/architects/view/WondersFrame.java
+++ src/main/java/jalau/at18/architects/view/WondersFrame.java
@@ -1,16 +1,26 @@
 package jalau.at18.architects.view;
 import static jalau.at18.architects.Constants.WonderFrame.*;
 import javax.swing.JFrame;
+import jalau.at18.architects.controller.Controller;
 
 public class WondersFrame extends JFrame {
-
+    private StartButton startButton;
+    private Controller controller;
+    private StartLabel startLabel;
+    private ChoiceNumberPlayers choiceNumberPlayers;
     public WondersFrame() {
-
+        controller = new Controller(this);
         initialize();
     }
     private void initialize() {
-        setBounds(DEFAULT_POS, DEFAULT_POS, DEFAULT_SIZE, DEFAULT_SIZE);
+        startLabel = new StartLabel();
+        startButton = new StartButton(controller);
+        choiceNumberPlayers = new ChoiceNumberPlayers();
+        add(startButton);
+        add(startLabel);
+        add(choiceNumberPlayers);
         setTitle(WINDOW_NAME);
+        setBounds(DEFAULT_POS, DEFAULT_POS, DEFAULT_SIZE, DEFAULT_SIZE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         setVisible(true);
