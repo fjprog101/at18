b987a30d28a6c29ec6d14f61a236dec1456ab0a4
Rodrigo Bernal <Rodrigo.Bernal@jala.university>
2022-09-21 15:39:36 -0400

    Solve checkstyle problems

--- src/main/java/jalau/at18/azul/TileColor.java
+++ src/main/java/jalau/at18/azul/TileColor.java
@@ -2,14 +2,14 @@ package jalau.at18.azul;
 
 public enum TileColor {
 
-    RED(false,"RED"),
-    YELLOW(false,"YELLOW"),
-    DARK(false,"DARK"),
-    BLUE(false,"BLUE"),
-    WHITE(false,"WHITE"),
-    FIRST_PLAYER(false,"FIRST"),
-    EMPTY(false,"EMPTY"),
-    NULL(false,"NULL");
+    RED(false, "RED"),
+    YELLOW(false, "YELLOW"),
+    DARK(false, "DARK"),
+    BLUE(false, "BLUE"),
+    WHITE(false, "WHITE"),
+    FIRST_PLAYER(false, "FIRST"),
+    EMPTY(false, "EMPTY"),
+    NULL(false, "NULL");
 
     private final boolean statusTile;
     private String name;
