7b145596e1a23a3d4522701c9dddfab19ac2034f
FernandezRobert <soniic9911@gmail.com>
2022-09-21 11:39:19 -0400

    gave button functionality to first player chooser

--- src/main/java/jalau/at18/kingoftokyo/view/choosefirstplayer/CenterPanel.java
+++ src/main/java/jalau/at18/kingoftokyo/view/choosefirstplayer/CenterPanel.java
@@ -1,5 +1,7 @@
 package jalau.at18.kingoftokyo.view.choosefirstplayer;
 
+import jalau.at18.kingoftokyo.view.chooseplayer.ChoosePlayersFrame;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,9 +10,9 @@ public class CenterPanel extends JPanel {
     private static final int DEFAULT_WIDTH = 100;
     private static final int DEFAULT_HEIGHT = 100;
 
-    public CenterPanel() {
+    public CenterPanel(ChooseFirstPlayerFrame frame) {
         FieldSetPanel fieldSetPanel = new FieldSetPanel();
-        ButtonsPanel buttonsPanel = new ButtonsPanel();
+        ButtonsPanel buttonsPanel = new ButtonsPanel(frame);
         initialize();
         add(fieldSetPanel, BorderLayout.CENTER);
         add(buttonsPanel, BorderLayout.SOUTH);
--- src/main/java/jalau/at18/kingoftokyo/view/choosefirstplayer/NextButton.java
+++ src/main/java/jalau/at18/kingoftokyo/view/choosefirstplayer/NextButton.java
@@ -1,10 +1,12 @@
 package jalau.at18.kingoftokyo.view.choosefirstplayer;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class NextButton extends JButton {
 
-    public NextButton() {
+    public NextButton(ActionListener listener) {
         super("Next");
+        addActionListener(listener);
     }
 }
