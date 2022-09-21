6e601c593e7846636de91a1390b2f7a50a25ad5e
Daniela Martinez <Daniela.Martinez@jala.university>
2022-09-21 15:42:48 -0400

    Create the class tilebutton and tilebuttongroup inside the view carpet

---
+++ src/main/java/jalau/at18/azul/view/TileButton.java
@@ -0,0 +1,12 @@
+package jalau.at18.azul.view;
+
+import java.awt.Color;
+import javax.swing.JToggleButton;
+
+public class TileButton extends JToggleButton {
+    public TileButton() {
+        setSize(10, 10);
+        setLocation(5, 5);
+        setBackground(Color.GRAY);
+    }
+}
