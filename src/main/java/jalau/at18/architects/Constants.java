bf62a70f5d5d5e359c0351ea5d3ea573f23bee48
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-20 20:21:52 -0400

    created the constants class with non-modifiable variables

---
+++ src/main/java/jalau/at18/architects/Constants.java
@@ -0,0 +1,10 @@
+package jalau.at18.architects;
+
+public final class Constants {
+
+    public final class WonderFrame {
+        public static final int DEFAULT_POS = 50;
+        public static final int DEFAULT_SIZE = 700;
+        public static final String WINDOW_NAME = "7 Wonders Architect";
+    }
+}
