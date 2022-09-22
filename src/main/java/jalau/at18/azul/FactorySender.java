9f8a497a9d9fe630a28fc5e7b1f5c20b86bddfa7
Rodrigo Bernal <Rodrigo.Bernal@jala.university>
2022-09-22 14:03:39 -0400

    Delete FactorySendertToPyramid class

--- src/main/java/jalau/at18/azul/FactorySender.java
+++ src/main/java/jalau/at18/azul/FactorySender.java
@@ -3,18 +3,15 @@ package jalau.at18.azul;
 public class FactorySender {
 
     private Factory factory;
-    private FactorySenderToPyramid factorySenderToPyramid;
     private FactorySenderToCenterBoard factorySenderToCenterBoard;
 
     public FactorySender(Factory factory, CenterTileBoard centerBoard) {
         this.factory = factory;
-        this.factorySenderToPyramid = new FactorySenderToPyramid();
         this.factorySenderToCenterBoard = new FactorySenderToCenterBoard(centerBoard);
     }
 
     public void sendTiles(Tile tileSelected) {
         factory.addTilesSelected(tileSelected);
-        factorySenderToPyramid.sendTilesToPyramid(factory.getTilesSelected());
         factory.removeTilesSelected(tileSelected);
         factorySenderToCenterBoard.sendTilesToCenterBoard(factory.getTiles());
         factory.getTiles().clear();
