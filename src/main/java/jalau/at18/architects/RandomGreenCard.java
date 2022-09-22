5433fe5e67fcf0c6c9e777730bd1e177a9d6c203
tonio <JoseRomay@jala.university>
2022-09-22 19:22:20 -0400

    Add the GreenCard generator and your test

---
+++ src/main/java/jalau/at18/architects/RandomGreenCard.java
@@ -0,0 +1,23 @@
+package jalau.at18.architects;
+
+public class RandomGreenCard {
+
+    public GreenCard getGreenCard() {
+        int rand = (int) (Math.random() * (2 - 0 + 1) + 0);
+        return new GreenCard(getSymbol(rand));
+    }
+
+    public ScientificSymbol getSymbol(int rand) {
+        if (rand == 0) {
+            return ScientificSymbol.COMPASS;
+        }
+        if (rand == 1) {
+            return ScientificSymbol.OLDTABLE;
+        }
+        if (rand == 2) {
+            return ScientificSymbol.SCREW;
+        }
+        return null;
+    }
+
+}
