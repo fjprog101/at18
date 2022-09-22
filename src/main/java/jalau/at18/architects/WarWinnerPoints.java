d06551f9e5e0b57ee417a4f746005411ecce4275
Carlos Calderon <Carlos.Calderon@Jala.University>
2022-09-21 21:28:40 -0400

    checkstyle fixed

--- src/main/java/jalau/at18/architects/WarWinnerPoints.java
+++ src/main/java/jalau/at18/architects/WarWinnerPoints.java
@@ -2,7 +2,7 @@ package jalau.at18.architects;
 
 public class WarWinnerPoints {
     private int warTokens;
-    private int pointForVictory = 3;
+    private static final int POINTFORVICTORY = 3;
 
     public WarWinnerPoints() {
         this.warTokens = 0;
@@ -14,6 +14,6 @@ public class WarWinnerPoints {
         return warTokens;
     }
     public int getPoints() {
-        return (pointForVictory * warTokens);
+        return (POINTFORVICTORY * warTokens);
     }
 }
