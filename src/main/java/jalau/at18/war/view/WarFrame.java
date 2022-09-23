4c80f09e57f325cb27104ace59f4a5ae85f514d4
Jose Guevara <Jose.Guevara@Jala.University>
2022-09-22 22:57:46 -0400

    Add world map to main frame

--- src/main/java/jalau/at18/war/view/WarFrame.java
+++ src/main/java/jalau/at18/war/view/WarFrame.java
@@ -1,13 +1,15 @@
 package jalau.at18.war.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
 
 import jalau.at18.war.model.Game;
 
 public class WarFrame extends JFrame {
-
-    private static final int DEFAULT_SIZE = 500;
 
     public WarFrame() {
         // Initialize controller with reference to this ui frame
@@ -23,8 +25,13 @@ public class WarFrame extends JFrame {
         add(new PlayerPanel(game), BorderLayout.WEST);
         add(new PlayerPanel(game), BorderLayout.SOUTH);
 
+        // Add world map
+        JLabel worldMap = new JLabel(new ImageIcon("src/main/resources/war/world-map.png"));
+        worldMap.setBorder(new LineBorder(Color.BLUE));
+        add(worldMap, BorderLayout.CENTER);
+
         // Configure frame
-        setSize(DEFAULT_SIZE, DEFAULT_SIZE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
--- src/test/java/jalau/at18/war/view/WarFrameTest.java
+++ src/test/java/jalau/at18/war/view/WarFrameTest.java
@@ -1,6 +1,7 @@
 package jalau.at18.war.view;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import javax.swing.JFrame;
@@ -9,10 +10,12 @@ import org.junit.Test;
 public class WarFrameTest {
 
     @Test
-    public void shouldHaveRollerAndDice() {
+    public void shouldHave4PlayersAndWorldMap() {
         WarFrame warFrame = new WarFrame();
-        assertEquals(4, warFrame.getContentPane().getComponentCount());
+        assertEquals(5, warFrame.getContentPane().getComponentCount());
         assertEquals(JFrame.EXIT_ON_CLOSE, warFrame.getDefaultCloseOperation());
+        assertEquals(JFrame.MAXIMIZED_BOTH, warFrame.getExtendedState());
+        assertNotNull(warFrame.getLayout());
         assertTrue(warFrame.isVisible());
     }
 }
