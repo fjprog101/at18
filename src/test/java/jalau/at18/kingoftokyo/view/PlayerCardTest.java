d2d3dc1d44f26a8e98ec35520d03e067bf809466
Sergio Lema <sergio.lema@jala.university>
2022-09-22 22:51:38 -0400

    Create test for playercard

---
+++ src/test/java/jalau/at18/kingoftokyo/view/PlayerCardTest.java
@@ -0,0 +1,31 @@
+package jalau.at18.kingoftokyo.view;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import jalau.at18.kingoftokyo.*;
+import jalau.at18.kingoftokyo.view.PlayerCards.*;
+
+import org.junit.Test;
+
+public class PlayerCardTest {
+    @Test
+    public void shouldShowACard() {
+        Player player = new Player();
+        PlayerCard PlayerPanel1 = new PlayerCard(Monster.ALIENOID, 0, player);
+        assertEquals(5, PlayerPanel1.getComponentCount());
+        assertEquals(20, PlayerPanel1.getBounds().getX(), 0);
+        assertEquals(20, PlayerPanel1.getBounds().getY(), 0);
+        assertEquals(300, PlayerPanel1.getBounds().getWidth(), 0);
+        assertEquals(150, PlayerPanel1.getBounds().getHeight(), 0);
+        assertTrue(PlayerPanel1.isVisible());
+
+        PlayerCard PlayerPanel2 = new PlayerCard(Monster.ALIENOID, 1, player);
+        assertEquals(5, PlayerPanel2.getComponentCount());
+        assertEquals(20, PlayerPanel2.getBounds().getX(), 0);
+        assertEquals(190, PlayerPanel2.getBounds().getY(), 0);
+        assertEquals(300, PlayerPanel2.getBounds().getWidth(), 0);
+        assertEquals(150, PlayerPanel2.getBounds().getHeight(), 0);
+        assertTrue(PlayerPanel2.isVisible());
+    }
+}
