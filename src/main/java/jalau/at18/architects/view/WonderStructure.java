2138893a370f6d67c6a358d9c41a97f2196a8abb
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-22 20:15:49 -0400

    Created class wonderStructure for draw the wonder on Frame

---
+++ src/main/java/jalau/at18/architects/view/WonderStructure.java
@@ -0,0 +1,17 @@
+package jalau.at18.architects.view;
+import java.awt.*;
+import javax.swing.*;
+import static jalau.at18.architects.Constants.WonderStructure.*;
+class WonderStructure extends JPanel {
+    public static final int HEIGHT = 100;
+    public static final int MIDDLE = 50;
+    public static final int TRIANGLE_TIPS = 3;
+    WonderStructure() {
+        setBounds(DEFAULT_POSX, DEFAULT_POSY, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+    public void paintComponent(Graphics g) {
+        int[] xPos = {MIDDLE, HEIGHT, 0};
+        int[] yPos = {0, HEIGHT, HEIGHT};
+        g.drawPolygon(xPos, yPos, TRIANGLE_TIPS);
+    }
+}
