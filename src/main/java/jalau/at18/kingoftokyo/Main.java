73d513eeabc468d15e20153568783ed1404a5140
Adriana Olivera <Adriana.Olivera@jala.university>
2022-09-23 02:29:34 -0400

    modifi main to display principal frame

--- src/main/java/jalau/at18/kingoftokyo/Main.java
+++ src/main/java/jalau/at18/kingoftokyo/Main.java
@@ -2,11 +2,8 @@ package jalau.at18.kingoftokyo;
 
 import jalau.at18.kingoftokyo.view.chooseplayer.ChoosePlayersFrame;
 
-import jalau.at18.kingoftokyo.view.GameFrame;
-//import jalau.at18.kingoftokyo.view.chooseplayer.ChoosePlayersFrame;
 public class Main {
     public static void main(String[] args) {
-        //new ChoosePlayersFrame();
-        new GameFrame();
+        new ChoosePlayersFrame();
     }
 }
