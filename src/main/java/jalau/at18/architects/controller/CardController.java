bec7f69b4eab2f3f7db1d7328a7b6392d40fe8a5
Libertad Tolaba <Libertad.Tolaba@jala.university>
2022-09-25 16:47:27 -0400

    Added the card controller for the middle deck of cards, rename DeckController to CardController

---
+++ src/main/java/jalau/at18/architects/controller/CardController.java
@@ -0,0 +1,49 @@
+package jalau.at18.architects.controller;
+
+import java.awt.event.MouseEvent;
+
+import javax.swing.event.MouseInputListener;
+
+import jalau.at18.architects.model.CardFace;
+import jalau.at18.architects.model.Deck;
+import jalau.at18.architects.view.CardLabel;
+
+public class CardController implements MouseInputListener {
+    private Deck deck;
+    private CardLabel card;
+    public CardController(CardLabel mazo) {
+        deck = new Deck();
+        card = mazo;
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        CardFace cardFace = deck.getRamdomCard();
+        String newImage = cardFace.getNameOfFile();
+        card.setImage(newImage);
+        System.out.println("Mouse usado");
+    }
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+}
