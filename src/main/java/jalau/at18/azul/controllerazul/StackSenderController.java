f373ddc29ee280a892fe8840d0b89c8a82eac4b0
Hugo Solares <Hugo.Solares@jala.university>
2022-09-28 18:07:23 -0400

    adding new mechanic were the pyramid doesn't accept different colors in a single row

--- src/main/java/jalau/at18/azul/controllerazul/StackSenderController.java
+++ src/main/java/jalau/at18/azul/controllerazul/StackSenderController.java
@@ -1,10 +1,12 @@
 package jalau.at18.azul.controllerazul;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
 import jalau.at18.azul.Tile;
+import jalau.at18.azul.TileColor;
 import jalau.at18.azul.view.FloorButtons;
 import jalau.at18.azul.view.PointerGroup;
 import jalau.at18.azul.view.PyramidBoard;
@@ -15,11 +17,13 @@ public class StackSenderController implements ActionListener {
     private PointerGroup pointerGroup;
     private PyramidBoard pyramidBoard;
     private FloorButtons floorButtons;
+
     private final int counter = 4;
     private int count;
     private List<Tile> colorPyramidTilesList = new ArrayList<Tile>();
 
-    public StackSenderController(StackButtonGroup newStack, PointerGroup newPointer, PyramidBoard newPyramid, FloorButtons newFloor) {
+    public StackSenderController(StackButtonGroup newStack, PointerGroup newPointer, PyramidBoard newPyramid,
+            FloorButtons newFloor) {
         count = counter;
         this.stackButtonGroup = newStack;
         this.pointerGroup = newPointer;
@@ -29,22 +33,43 @@ public class StackSenderController implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        Tile emptyTile = new Tile(TileColor.EMPTY);
+        boolean pyramidplay = false;
         stackButtonGroup.sentStackColorList();
         for (int index = 0; index < pointerGroup.getComponentCount(); index++) {
             if (e.getSource() == (pointerGroup.getComponent(index))) {
                 count = pyramidBoard.sendEmptyTiles(index);
                 for (int jindex = 0; jindex < stackButtonGroup.getStackColorList().size(); jindex++) {
                     if (pyramidBoard.countEmptyPyramidTiles(index) > 0) {
-                        pyramidBoard.updateTiles(index, count, stackButtonGroup.getStackColorList().get(jindex));
-                        count--;
+                        if (pyramidBoard.getTileButton()[index][counter].getTileValue() == emptyTile.getColor().getName()) {
+                            pyramidBoard.updateTiles(index, count, stackButtonGroup.getStackColorList().get(jindex));                    
+                            count--;
+                            pyramidplay = true;
+                        } else {
+                            if (pyramidBoard.getTileButton()[index][counter].getTileValue() == stackButtonGroup.getStackColorList().get(0).getColor().getName()) {
+                                pyramidBoard.updateTiles(index, count, stackButtonGroup.getStackColorList().get(jindex));                    
+                                count--;
+                                pyramidplay = true;
+                            } else {
+                                colorPyramidTilesList.clear();
+                            }
+                        }
                     } else {
-                        colorPyramidTilesList.add(stackButtonGroup.getStackColorList().get(jindex));
+                        if (pyramidplay){
+                            colorPyramidTilesList.add(stackButtonGroup.getStackColorList().get(jindex));
+
+                        }
+                        //pyramidplay = true;
                     }
                 }
             }
         }
         floorButtons.updateButtonGroup(colorPyramidTilesList);
         count = counter;
-        stackButtonGroup.clearStackList();
+        colorPyramidTilesList.clear();
+        if (pyramidplay)
+        {
+            stackButtonGroup.clearStackList();
+        }
     }
 }
--- src/main/java/jalau/at18/azul/view/PyramidPointer.java
+++ src/main/java/jalau/at18/azul/view/PyramidPointer.java
@@ -11,6 +11,7 @@ public class PyramidPointer extends JButton {
     public static final int DEFAULT_POS1 = 400;
 
     public PyramidPointer(ActionListener listener) {
+        
         setSize(DEFAULT_POS1, DEFAULT_POS1);
         setLocation(CENTER, CENTER);
         Icon icon = new ImageIcon("src/main/resources/azul/triangle.png");
