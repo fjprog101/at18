0b521e0be8509873374a3f538cae296fe616d682
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-28 11:57:37 -0400

    added method to be able to test the class and added its unitest for the class GenerateARandomBluuePointForBluePOints

--- src/test/java/jalau/at18/architects/model/cards/GenerateARandomBluePointForBluePointsTest.java
+++ src/test/java/jalau/at18/architects/model/cards/GenerateARandomBluePointForBluePointsTest.java
@@ -1,12 +1,13 @@
 package jalau.at18.architects.model.cards;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
 public class GenerateARandomBluePointForBluePointsTest {
     @Test
     public void shouldHaveBluePointsWithCatIncludedOrNot(){
-        int quantityRandom = 0;
-        while(quantityRandom < 4) {
+
         GenerateARandomBluePointForBluePoints generateARandomBluePointForBluePoints = new GenerateARandomBluePointForBluePoints();
         BluePoints bluePoints = generateARandomBluePointForBluePoints.randomBluePoint();
         int catPoints = bluePoints.getCats();
@@ -14,13 +15,43 @@ public class GenerateARandomBluePointForBluePointsTest {
         if(catPoints == 1){
             assertEquals(1, catPoints);
             assertEquals(2, victoryPoints);
-            quantityRandom ++;
         } else {
             assertEquals(0, catPoints);
             assertEquals(3, victoryPoints);
-            quantityRandom ++;
         }
     }
-}
+    @Test
+    public void shouldHaveBluePointsWithCatIncluded() {
+        GenerateARandomBluePointForBluePoints generateARandomBluePointForBluePoints = new GenerateARandomBluePointForBluePoints();
+        generateARandomBluePointForBluePoints.setRandonNumber(2);
+        BluePoints bluePoints = generateARandomBluePointForBluePoints.randomBluePoint();
+        int catPoints = bluePoints.getCats();
+        int victoryPoints = bluePoints.getPoints();
+        assertEquals(1, catPoints);
+        assertEquals(2, victoryPoints);
+    }
+    @Test
+    public void shouldHaveBluePointsWithCatNotIncluded() {
+        GenerateARandomBluePointForBluePoints generateARandomBluePointForBluePoints = new GenerateARandomBluePointForBluePoints();
+        generateARandomBluePointForBluePoints.setRandonNumber(3);
+        BluePoints bluePoints = generateARandomBluePointForBluePoints.randomBluePoint();
+        int catPoints = bluePoints.getCats();
+        int victoryPoints = bluePoints.getPoints();
+        assertEquals(0, catPoints);
+        assertEquals(3, victoryPoints);
+    }
+
+    @Test
+    public void shouldHaveBluePointsWithOutPointsAndCat() {
+        GenerateARandomBluePointForBluePoints generateARandomBluePointForBluePoints = new GenerateARandomBluePointForBluePoints();
+        generateARandomBluePointForBluePoints.setRandonNumber(5);
+        BluePoints bluePoints = generateARandomBluePointForBluePoints.randomBluePoint();
+        int catPoints = bluePoints.getCats();
+        int victoryPoints = bluePoints.getPoints();
+        assertEquals(0, catPoints);
+        assertEquals(0, victoryPoints);
+    }
 
 }
+
+
