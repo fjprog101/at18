c45c16d5ab50c842d6d18f454e533936fd72bb6c
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-28 11:34:02 -0400

    added method to be able to test the class and added its unitest for the class

--- src/test/java/jalau/at18/architects/model/cards/GenerateARandomCardForResourceListTest.java
+++ src/test/java/jalau/at18/architects/model/cards/GenerateARandomCardForResourceListTest.java
@@ -8,32 +8,54 @@ import org.junit.Test;
 public class GenerateARandomCardForResourceListTest {
     @Test
     public void shouldBeARandomResourceCard(){
-        int quantityRandom = 0;
-        while(quantityRandom < 20) {
+
+
         GenerateARandomCardForResourceList generateARandomCardForResourceList = new GenerateARandomCardForResourceList();
         Card randomCard = generateARandomCardForResourceList.randomResourceCard();
         int randomNum = generateARandomCardForResourceList.getRandomNumber();
         if(randomNum == 1){
             assertEquals("Gray", randomCard.getColor());
-            quantityRandom++;
+
         } else if (randomNum == 2){
             assertEquals("Gray", randomCard.getColor());
-            quantityRandom++;
+
         } else if (randomNum == 3){
             assertEquals("Gray", randomCard.getColor());
-            quantityRandom++;
+
         } else if (randomNum == 4){
             assertEquals("Gray", randomCard.getColor());
-            quantityRandom++;
+
         } else if (randomNum == 5){
             assertEquals("Gray", randomCard.getColor());
-            quantityRandom++;
+
         } else if (randomNum == 6){
             assertEquals("Yellow", randomCard.getColor());
-            quantityRandom++;
+
         } else {
             assertNull(randomCard);
         }
+
     }
-}
+    @Test
+    public void shouldReturnDifferentsCards(){
+
+        GenerateARandomCardForResourceList generateARandomCardForResourceList = new GenerateARandomCardForResourceList();
+        generateARandomCardForResourceList.setRandomNumber(1);
+        assertEquals(EnumResourceList.WATER.getCard(), generateARandomCardForResourceList.randomResourceCard());
+
+        generateARandomCardForResourceList.setRandomNumber(2);
+        assertEquals(EnumResourceList.WOOD.getCard(), generateARandomCardForResourceList.randomResourceCard());
+
+        generateARandomCardForResourceList.setRandomNumber(3);
+        assertEquals(EnumResourceList.STONE.getCard(), generateARandomCardForResourceList.randomResourceCard());
+
+        generateARandomCardForResourceList.setRandomNumber(4);
+        assertEquals(EnumResourceList.PAPER.getCard(), generateARandomCardForResourceList.randomResourceCard());
+
+        generateARandomCardForResourceList.setRandomNumber(5);
+        assertEquals(EnumResourceList.BRICK.getCard(), generateARandomCardForResourceList.randomResourceCard());
+
+        generateARandomCardForResourceList.setRandomNumber(6);
+        assertEquals(EnumResourceList.GOLD.getCard(), generateARandomCardForResourceList.randomResourceCard());
+    }
 }
