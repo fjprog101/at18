0c94fd3997ea2b35a5c9e2f2456bbc3d6432f3ae
Rodrigo Valda <Rodrigo.Valda@jala.university>
2022-09-29 13:57:49 -0400

    Fix floor buttons logic

--- src/main/java/jalau/at18/azul/controllerazul/StackSenderController.java
+++ src/main/java/jalau/at18/azul/controllerazul/StackSenderController.java
@@ -2,16 +2,13 @@ package jalau.at18.azul.controllerazul;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-<<<<<<< HEAD
 
 
 import jalau.at18.azul.Floor;
-=======
 import java.util.ArrayList;
 import java.util.List;
 import jalau.at18.azul.Tile;
 import jalau.at18.azul.TileColor;
->>>>>>> d2ae7b82 (Fix pyramid add tiles logic)
 import jalau.at18.azul.view.FloorButtons;
 import jalau.at18.azul.view.PointerGroup;
 import jalau.at18.azul.view.PyramidBoard;
@@ -25,12 +22,7 @@ public class StackSenderController implements ActionListener {
     private int count;
     private Floor colorPyramidTilesList;
 
-<<<<<<< HEAD
     public StackSenderController(StackButtonGroup newStack, PointerGroup newPointer, PyramidBoard newPyramid, FloorButtons newFloor, Floor floort) {
-=======
-   public StackSenderController(StackButtonGroup newStack, PointerGroup newPointer, PyramidBoard newPyramid,
-            FloorButtons newFloor) {
->>>>>>> d2ae7b82 (Fix pyramid add tiles logic)
         count = counter;
         this.stackButtonGroup = newStack;
         this.pointerGroup = newPointer;
@@ -44,11 +36,13 @@ public class StackSenderController implements ActionListener {
         Tile emptyTile = new Tile(TileColor.EMPTY);
         boolean pyramidplay = false;
         stackButtonGroup.sentStackColorList();
-        System.out.println(stackButtonGroup.getStackColorList().size());
         for (int index = 0; index < pointerGroup.getComponentCount(); index++) {
             if (e.getSource() == (pointerGroup.getComponent(index))) {
                 count = pyramidBoard.sendEmptyTiles(index);
-                for (int jindex = 0; jindex < stackButtonGroup.getStackColorList().size(); jindex++) {
+                int tilesLimit = stackButtonGroup.getStackColorList().size();
+                for (int jindex = 0; jindex < tilesLimit; jindex++) {
+                    System.out.println("Lista: "+stackButtonGroup.getStackColorList().size());
+                    System.out.println("Index: " + jindex);
                     if (pyramidBoard.countEmptyPyramidTiles(index) > 0) {
                         if ((pyramidBoard.getTileButton()[index][counter].getTileValue() == emptyTile.getColor().getName()) || (pyramidBoard.getTileButton()[index][counter].getTileValue() == stackButtonGroup.getStackColorList().get(0).getColor().getName())) {
                             pyramidBoard.updateTiles(index, count, stackButtonGroup.getStackColorList().get(jindex));
@@ -56,19 +50,17 @@ public class StackSenderController implements ActionListener {
                             pyramidplay = true;
                         } else {
                             stackButtonGroup.getStackColorList().clear();
-                            
                         }
                     } else {
                         System.out.println("Entro");
                         colorPyramidTilesList.add(stackButtonGroup.getStackColorList().get(jindex));
                         stackButtonGroup.clearStackList();
-                        stackButtonGroup.getStackColorList().clear();
                     }
                 }
+                stackButtonGroup.getStackColorList().clear();
             }
             floorButtons.updateButtonGroup(colorPyramidTilesList);
             count = counter;
-            colorPyramidTilesList.clear();
             if (pyramidplay) {
                 stackButtonGroup.clearStackList();
                 stackButtonGroup.getStackColorList().clear();
