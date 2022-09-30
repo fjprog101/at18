beb6d9c235140ab8acd4ec28294dfb89aab0ca68
tonio <JoseRomay@jala.university>
2022-09-30 16:22:00 -0400

    Board Frame Test was fixed

--- src/test/java/jalau/at18/architects/view/BoardFrameTest.java
+++ src/test/java/jalau/at18/architects/view/BoardFrameTest.java
@@ -8,7 +8,7 @@ public class BoardFrameTest {
     @Test
     public void boardFrameShouldShowByDefault() {
         BoardFrame boardFrame = new BoardFrame("3");
-        assertEquals(1000, boardFrame.getWidth());
-        assertEquals(600, boardFrame.getHeight());
+        assertEquals(840, boardFrame.getWidth());
+        assertEquals(780, boardFrame.getHeight());
     }
 }
