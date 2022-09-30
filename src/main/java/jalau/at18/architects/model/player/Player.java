8edd5b45607d783ec1689de90c1c852f259a8f4d
Libertad Tolaba <Libertad.Tolaba@jala.university>
2022-09-30 10:09:33 -0400

    The Game class was created, the Singleton pattern with inner class was used, a Player attribute was modified to receive its wonder

--- src/main/java/jalau/at18/architects/model/player/Player.java
+++ src/main/java/jalau/at18/architects/model/player/Player.java
@@ -1,13 +1,13 @@
 package jalau.at18.architects.model.player;
 
 import jalau.at18.architects.model.cards.Card;
-import jalau.at18.architects.model.wonders.Wonders;
+import jalau.at18.architects.model.wonders.Wonder;
 
 public class Player {
     private String name;
-    private Wonders wonder;
+    private Wonder wonder;
     private Playcard playcard;
-    public Player(String name, Wonders wonder) {
+    public Player(String name, Wonder wonder) {
         this.name = name;
         this.wonder = wonder;
         playcard = new Playcard();
@@ -17,7 +17,7 @@ public class Player {
         return name;
     }
 
-    public Wonders getWonder() {
+    public Wonder getWonder() {
         return wonder;
     }
 
