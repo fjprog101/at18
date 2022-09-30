5d061aada60f3eace32babf99ec9396d62bae344
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-09-29 20:26:33 -0400

    Unit test to showDicesPanel and showResultsPanel

---
+++ src/test/java/jalau/at18/kingoftokyo/view/PanelSouthTest.java
@@ -0,0 +1,47 @@
+package jalau.at18.kingoftokyo.view;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+import jalau.at18.kingoftokyo.controller.WhoStartRollDiceController;
+import jalau.at18.kingoftokyo.model.Player;
+import jalau.at18.kingoftokyo.view.rolldicesection.RollDiceSectionUI;
+import jalau.at18.kingoftokyo.view.whostart.PanelSouth;
+import jalau.at18.kingoftokyo.view.whostart.ShowResultsPanel;
+import jalau.at18.kingoftokyo.view.whostart.StartGameButton;
+import jalau.at18.kingoftokyo.view.whostart.WhoStartFrame;
+
+
+public class PanelSouthTest {/* 
+    @Test
+    public void shouldShowACard() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+
+
+        ArrayList<Player> playerList = new ArrayList<Player>();
+        playerList.add(player3);
+        playerList.add(player1);
+        playerList.add(player2);
+
+        WhoStartFrame whoStartFrame = new WhoStartFrame(playerList);
+
+        RollDiceSectionUI rollDiceSectionUI = new RollDiceSectionUI();
+        ShowResultsPanel showResultsPanel = new ShowResultsPanel(playerList);
+        //StartGameButton startGameButton = new StartGameButton(null); //whoStartRollDiceController
+        
+        WhoStartRollDiceController whoStartRollDiceController = new WhoStartRollDiceController(whoStartFrame, rollDiceSectionUI, showResultsPanel, startGameButton);
+        PanelSouth panelSouth = new PanelSouth(whoStartRollDiceController);
+        //assertEquals(300, panelSouth.getBounds().x, 0);
+        //assertEquals(400, panelSouth.getBounds().getHeight(), 0);
+        assertEquals(600, panelSouth.getBounds().getWidth(), 0);
+        assertEquals(200, panelSouth.getBounds().getHeight(), 0);
+
+
+    }*/
+}
---
+++ src/test/java/jalau/at18/kingoftokyo/view/ShowResultsPanelTest.java
@@ -0,0 +1,30 @@
+package jalau.at18.kingoftokyo.view;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import jalau.at18.kingoftokyo.model.Monster;
+import jalau.at18.kingoftokyo.model.Player;
+import jalau.at18.kingoftokyo.view.whostart.ShowResultsPanel;
+
+public class ShowResultsPanelTest {
+    @Test
+    public void positionShouldResultsPanel() {
+        Player player1 = new Player();
+        player1.setMonster(Monster.ALIENOID);
+
+        ArrayList<Player> playerList = new ArrayList<Player>();
+        playerList.add(player1);
+
+        ShowResultsPanel showResultsPanel = new ShowResultsPanel(playerList);
+        assertEquals(650, showResultsPanel.getBounds().getX(), 0);
+        assertEquals(50, showResultsPanel.getBounds().getY(), 0);
+
+        assertEquals(300, showResultsPanel.getBounds().getWidth(), 0);
+        assertEquals(200, showResultsPanel.getBounds().getHeight(), 0);
+
+    }
+}
