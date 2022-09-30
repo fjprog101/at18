67f61fc423fd68fc6b3016dbe211d3ce5bd0281a
Alvaro Sivila <alvaro.sivila@jala.university>
2022-09-30 17:05:25 -0400

    added final variables to fix the magic number problem

--- src/main/java/jalau/at18/architects/controller/Controller.java
+++ src/main/java/jalau/at18/architects/controller/Controller.java
@@ -1,13 +1,12 @@
 package jalau.at18.architects.controller;
 import jalau.at18.architects.model.Game;
 import jalau.at18.architects.view.WondersFrame;
-
-import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Controller implements ActionListener {
     private WondersFrame wondersFrame;
+    private static final int QUANTITY_COMPONENTS = 4;
     private boolean pressedCheckBox = false;
     public Controller(WondersFrame frame) {
         this.wondersFrame = frame;
@@ -23,8 +22,8 @@ public class Controller implements ActionListener {
         } else if (e.getSource() == wondersFrame.getChoice()) {
             wondersFrame.setLabels("Game players: " + wondersFrame.getChoice().getSelectedItem().toString());
             pressedCheckBox = true;
-            if (wondersFrame.getContentPane().getComponentCount() == 4) {
-                wondersFrame.getContentPane().remove(3);
+            if (wondersFrame.getContentPane().getComponentCount() == QUANTITY_COMPONENTS) {
+                wondersFrame.getContentPane().remove(QUANTITY_COMPONENTS - 1);
             }
             wondersFrame.addNewComponents(Integer.parseInt(wondersFrame.getChoice().getSelectedItem().toString()));
             wondersFrame.repaint();
