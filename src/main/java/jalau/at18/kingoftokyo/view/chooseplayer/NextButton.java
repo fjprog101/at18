23e824bbc615b5d35330c7962fa1f8af8ca3fff1
FernandezRobert <soniic9911@gmail.com>
2022-09-30 17:17:37 -0400

    Improved the UI of players chooser

--- src/main/java/jalau/at18/kingoftokyo/view/chooseplayer/NextButton.java
+++ src/main/java/jalau/at18/kingoftokyo/view/chooseplayer/NextButton.java
@@ -5,11 +5,18 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class NextButton extends JButton {
+    private static final int THREE = 3;
+    private static final int RED = 249;
+    private static final int GREEN = 156;
+    private static final int BLUE = 0;
     public static final int DEFAULT_WIDTH = 100;
     public static final int DEFAULT_HEIGHT = 40;
 
     public NextButton(ActionListener listener) {
         super("Next");
+        float[] hsb = new float[THREE];
+        hsb = Color.RGBtoHSB(RED, GREEN, BLUE, hsb);
+        setBackground(Color.getHSBColor(hsb[0], hsb[1], hsb[2]));
         setPreferredSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
         addActionListener(listener);
     }
