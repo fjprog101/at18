95dab092db4427fcb61c9ce63ad4d1522c064d28
tonio <JoseRomay@jala.university>
2022-10-02 23:54:23 -0400

    Points Wonder Added

--- src/main/java/jalau/at18/architects/model/wonders/Wonder.java
+++ src/main/java/jalau/at18/architects/model/wonders/Wonder.java
@@ -4,13 +4,28 @@ public abstract class Wonder {
     private final int maxOfStages = 5;
     private Stage[] stages;
     private String name;
+    private int points = 0;
+
     public Wonder() {
         stages = new Stage[maxOfStages];
     }
+
     public Stage[] getStages() {
         return stages;
     }
+
     abstract void fillStages();
+
+    public int getPointsWonder() {
+        // Wrong here
+        for (Stage stage : getStages()) {
+            if (stage.isBuilt()) {
+                this.points += stage.getPoints();
+                System.out.println(">>>>--->" + stage.getPoints());
+            }
+        }
+        return this.points;
+    }
 
     public String getName() {
         return name;
