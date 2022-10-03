b4e3f5ed5a390417bfdcbcc3d59853609fa7d3d9
tonio <JoseRomay@jala.university>
2022-10-02 23:51:24 -0400

    player points added

--- src/main/java/jalau/at18/architects/model/player/Player.java
+++ src/main/java/jalau/at18/architects/model/player/Player.java
@@ -7,10 +7,17 @@ public class Player {
     private String name;
     private Wonder wonder;
     private Playcard playcard;
+    private int playerPoints = 0;
+
     public Player(String name, Wonder wonder) {
         this.name = name;
         this.wonder = wonder;
         playcard = new Playcard();
+    }
+
+    public int getPointsPlayer() {
+
+        return this.playerPoints + playcard.getPointsPlaycard() + wonder.getPointsWonder();
     }
 
     public String getName() {
